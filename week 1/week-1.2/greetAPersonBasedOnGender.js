a5adfc38738dc25061304b56b8e6b2bb9cb1d14c
anoopraju31 <anoopraju31@gmail.com>
2023-12-05 16:27:10 +0530

    Week 1.2 - if else statement - greet a person given their first and last name and gender

---
+++ week 1/week-1.2/greetAPersonBasedOnGender.js
@@ -0,0 +1,9 @@
+let firstName = 'Anoop'
+let lastName = 'Raju'
+let gender = 'male'
+
+if (gender === 'male')
+	console.log('Greetings Mr. ' + firstName + ' ' + lastName)
+else if (gender === 'female')
+	console.log('Greetings Ms. ' + firstName + ' ' + lastName)
+else console.log('Greetings ' + firstName + ' ' + lastName)
