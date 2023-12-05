34bd595a326759574baa0fa8e0cdb6898ceb1770
anoopraju31 <anoopraju31@gmail.com>
2023-12-05 21:52:29 +0530

    Week 1.2 - function - sum of two numbers with additonal function to display result.

---
+++ week 1/week-1.2/sumOfTwoNumbersWithAdditonalFunctionToDisplayResult.js
@@ -0,0 +1,8 @@
+const sum = (a, b) => a + b
+const displayResult = (a, b, res) =>
+	console.log(`sum of ${a} and ${b} is ${res}`)
+
+let a = 10
+let b = 4
+let res = sum(a, b)
+displayResult(a, b, res)
