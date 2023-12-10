c75388a61355784e6156d258440eed4a559373c5
anoopraju31 <anoopraju31@gmail.com>
2023-12-10 13:37:23 +0530

    Week 1 - offline class - 1: level 2 : date

--- week 1/offline-classes/level 2/date.js
+++ week 1/offline-classes/level 2/date.js
@@ -1,17 +1,17 @@
 const currentDate = new Date()
 
-console.log('Current Date: ', currentDate)
+console.log('Current Date : ', currentDate)
 
 // * get functions
-console.log('Time   : ', currentDate.getTime()) // * epoch timestamp number of seconds past after 1970
-console.log('Date   : ', currentDate.getDate())
-console.log('Month  : ', currentDate.getMonth()) // 0 - 11
-console.log('Year   : ', currentDate.getFullYear()) // 2023
-console.log('Year   : ', currentDate.getYear()) // 2023 - 1900
-console.log('Day    : ', currentDate.getDay()) // 0 - 6
-console.log('Minute : ', currentDate.getMinutes())
-console.log('Seconds: ', currentDate.getSeconds())
-console.log('Hours  : ', currentDate.getHours())
+console.log('Time         : ', currentDate.getTime()) // * epoch timestamp number of seconds past after 1970
+console.log('Date         : ', currentDate.getDate())
+console.log('Month        : ', currentDate.getMonth()) // 0 - 11
+console.log('Year         : ', currentDate.getFullYear()) // 2023
+console.log('Year         : ', currentDate.getYear()) // 2023 - 1900
+console.log('Day          : ', currentDate.getDay()) // 0 - 6
+console.log('Minute       : ', currentDate.getMinutes())
+console.log('Seconds      : ', currentDate.getSeconds())
+console.log('Hours        : ', currentDate.getHours())
 
 // * set functions
 currentDate.setFullYear(2022)
