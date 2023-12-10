fbe6b0b43731d7dfa8417aa5d2288dc0e3a807c5
anoopraju31 <anoopraju31@gmail.com>
2023-12-10 11:50:11 +0530

    Week 1 - offline class - 1: level 2 : class

--- week 1/offline-classes/level 2/class.js
+++ week 1/offline-classes/level 2/class.js
@@ -8,3 +8,11 @@ class Animal {
 		return `${this.name} has ${this.legCount} legs.`
 	}
 }
+
+const dog = new Animal('Dog', 4)
+const cat = new Animal('Cat', 4)
+const spider = new Animal('Spider', 8)
+
+console.log(dog.describe())
+console.log(cat.describe())
+console.log(spider.describe())
