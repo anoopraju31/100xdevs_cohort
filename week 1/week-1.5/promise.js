80c04ba09797ddf2d52ab3b1dc3fe275a64ac900
anoopraju31 <anoopraju31@gmail.com>
2023-12-11 21:59:21 +0530

    Week 1.5 promise

--- week 1/week-1.5/promise.js
+++ week 1/week-1.5/promise.js
@@ -1,8 +1,15 @@
 const fs = require('fs')
 
 function AnoopFileRead() {
-	return new Promise(function (resolve) {
+	console.log('Inside AnoopFileRead')
+	return new Promise(function (resolve, reject) {
+		console.log('Inside Promise')
 		fs.readFile('a.txt', 'utf-8', function (error, data) {
+			if (error) {
+				console.log('error')
+				reject(error)
+			}
+			console.log('Before Resolve')
 			resolve(data)
 		})
 	})
@@ -12,4 +19,10 @@ function onDone(data) {
 	console.log(data)
 }
 
-AnoopFileRead().then(onDone)
+// AnoopFileRead().then(onDone)
+
+let a = AnoopFileRead()
+
+console.log(a)
+
+a.then(onDone)
