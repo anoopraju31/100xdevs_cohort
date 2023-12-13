b6496e25155a4acc5ef981aab9a7cd19bcf813f6
anoopraju31 <anoopraju31@gmail.com>
2023-12-13 12:57:48 +0530

    week 2.2 todo-app-server: added a mock database using json and fs/promise

--- week 2/week-2.2/todo-app-server/index.js
+++ week 2/week-2.2/todo-app-server/index.js
@@ -1,28 +1,66 @@
 const express = require('express')
+const fs = require('fs/promises')
 
 const app = express()
 const PORT = 5500
 
-const todos = []
-
 app.use(express.json())
 
-app.get('/', (req, res) => {
-	res.json({
-		status: 200,
-		todos,
-	})
+app.get('/', async (req, res) => {
+	try {
+		const data = await fs.readFile('todo.json', 'utf-8')
+		const { todos } = await JSON.parse(data)
+
+		console.log(todos)
+
+		return res.json({
+			status: 200,
+			todos,
+		})
+	} catch (error) {
+		console.error(error.message)
+
+		return res.status(500).json({
+			status: 500,
+			message: 'todo not found.',
+		})
+	}
 })
 
-app.post('/', (req, res) => {
+app.post('/', async (req, res) => {
 	const { todo } = req.body
 
-	todos.push(todo)
+	try {
+		const data = await fs.readFile('todo.json', 'utf-8')
+		const jsonData = await JSON.parse(data)
 
-	res.json({
-		status: 200,
-		message: 'todo created successfully.',
-	})
+		jsonData.todos.push(todo)
+
+		const stringifiedData = JSON.stringify(jsonData)
+
+		fs.writeFile('todo.json', stringifiedData, 'utf-8')
+			.then(() => {
+				return res.json({
+					status: 200,
+					message: 'todo created successfully.',
+				})
+			})
+			.catch((error) => {
+				console.error(error.message)
+
+				return res.status(500).json({
+					status: 500,
+					message: 'unable to add todo.',
+				})
+			})
+	} catch (error) {
+		console.error(error.message)
+
+		return res.status(500).json({
+			status: 500,
+			message: 'todo not found.',
+		})
+	}
 })
 
 app.listen(PORT, () => {
