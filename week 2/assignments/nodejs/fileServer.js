fc8a14239eadb71f3dfaa92ca83c5b7c601129b0
anoopraju31 <anoopraju31@gmail.com>
2023-12-14 21:09:35 +0530

    Week 2 Assignment: nodejs - file server

--- week 2/assignments/nodejs/fileServer.js
+++ week 2/assignments/nodejs/fileServer.js
@@ -12,10 +12,68 @@
     - For any other route not defined in the server return 404
     Testing the server - run `npm run test-fileServer` command in terminal
  */
-const express = require('express');
-const fs = require('fs');
-const path = require('path');
-const app = express();
+const express = require('express')
+const fs = require('fs/promises')
+const path = require('path')
+const app = express()
 
+async function getFileNames() {
+	try {
+		const files = await fs.readdir(path.join(__dirname, './files/'))
+		return files
+	} catch (error) {
+		console.error(error.message)
+		throw new Error('Unable to read files directory')
+	}
+}
 
-module.exports = app;
+async function getFileContents(filename) {
+	try {
+		const data = await fs.readFile(`files/${filename}`, 'utf-8')
+
+		return data
+	} catch (error) {
+		console.error(error.message)
+		throw new Error('Unable to read the file')
+	}
+}
+
+app.get('/files', async (req, res) => {
+	// try {
+	// 	const files = await getFileNames()
+
+	// 	res.json(files)
+	// } catch (error) {
+	// 	res.status(500).json({ error: 'Failed to retrieve files' })
+	// }
+
+	try {
+		const files = await fs.promises.readdir(path.join(__dirname, './files/'))
+		res.json(files)
+	} catch (error) {
+		console.error(error.message)
+		return res.status(500).json({ error: 'Failed to retrieve files' })
+	}
+})
+
+app.get('/file/:filename', async (req, res) => {
+	try {
+		const files = await getFileNames()
+		const filename = req.params.filename
+		const idx = files.indexOf(filename)
+
+		if (idx === -1) return res.status(404).send('File not found')
+
+		const data = await getFileContents(filename)
+
+		res.send(data)
+	} catch (error) {
+		res.sendStatus(500)
+	}
+})
+
+app.use((req, res) => {
+	res.status(404).send('Route not found')
+})
+
+module.exports = app
