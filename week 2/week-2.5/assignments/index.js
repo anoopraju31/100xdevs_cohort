15d61556d6b93011808177f8611919468b8dce35
anoopraju31 <anoopraju31@gmail.com>
2023-12-14 11:43:29 +0530

    Week 2.5 assigment - GET route created

--- week 2/week-2.5/assignments/index.js
+++ week 2/week-2.5/assignments/index.js
@@ -3,6 +3,37 @@ const express = require('express')
 const app = express()
 const PORT = 5500
 
+const users = [
+	{
+		name: 'John Doe',
+		kidneys: [
+			{
+				healthy: false,
+			},
+		],
+	},
+]
+
+app.get('/', (req, res) => {
+	const johnsKidneys = users[0].kidneys
+	const numberOfKidneys = johnsKidneys.length
+	let numberOfHealthyKidneys = 0
+
+	for (let i = 0; i < numberOfKidneys; i++) {
+		if (johnsKidneys[i].healthy) {
+			numberOfHealthyKidneys += 1
+		}
+	}
+
+	const numberOfUnHealthyKidneys = numberOfKidneys - numberOfHealthyKidneys
+
+	res.json({
+		numberOfKidneys,
+		numberOfHealthyKidneys,
+		numberOfUnHealthyKidneys,
+	})
+})
+
 app.listen(PORT, () => {
 	console.log(`Server Started at port ${PORT}`)
 })
