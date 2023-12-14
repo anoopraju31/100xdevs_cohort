b7018b93e9285f0e02f8016ba2985d31f1653761
anoopraju31 <anoopraju31@gmail.com>
2023-12-14 10:10:07 +0530

    Week 2.5 express http server tutorial - get route that send the sum of first n natural numbers

--- week 2/week-2.5/index.js
+++ week 2/week-2.5/index.js
@@ -3,10 +3,22 @@ const express = require('express')
 const app = express()
 const PORT = 5500
 
+function sum(n) {
+	let ans = 0
+
+	for (let i = 0; i <= n; i++) ans += i
+
+	return ans
+}
+
 app.get('/', (req, res) => {
+	const { n } = req.query
+	let value = sum(n ? n : 10000)
 	res.json({
 		status: 200,
 		message: 'Welcome to my server!',
+		number: n,
+		sum: value,
 	})
 })
 
