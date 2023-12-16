cf2a32bf41b946581cab7418c86dde2d299774bf
anoopraju31 <anoopraju31@gmail.com>
2023-12-16 19:47:46 +0530

    Week 3.1 middleware-tutorial: server created - with middlewares

--- week 3/week-3.1/middlewares/index.js
+++ week 3/week-3.1/middlewares/index.js
@@ -3,21 +3,26 @@ const express = require('express')
 const app = express()
 const PORT = 5500
 
-app.get('/health-checkup', function (req, res) {
+function userMiddleware(req, res, next) {
 	const username = req.headers.username
 	const password = req.headers.password
+
+	if (username != 'harkirat' || password != 'pass')
+		return res.status(400).json({ msg: 'Somethings up with your inputs' })
+
+	next()
+}
+
+function kidneyCheck(req, res, next) {
 	const kidneyId = req.query.kidneyId
 
-	if (username != 'harkirat' || password != 'pass') {
-		res.status(400).json({ msg: 'Somethings up with your ionputs' })
-		return
-	}
+	if (kidneyId != 1 && kidneyId != 2)
+		return res.status(400).json({ msg: 'Somethings up with your inputs' })
 
-	if (kidneyId != 1 && kidneyId != 2) {
-		res.status(400).json({ msg: 'Somethings up with your ionputs' })
-		return
-	}
-	// do something with kidney here
+	next()
+}
+
+app.get('/health-checkup', userMiddleware, kidneyCheck, function (req, res) {
 	res.json({
 		msg: 'Your kidney is fine!',
 	})
