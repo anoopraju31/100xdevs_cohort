5b36e59f1705053574192dcc24cbd53e74c55017
anoopraju31 <anoopraju31@gmail.com>
2023-12-17 08:35:16 +0530

    Week 3.1 Assignment: Calculate the average time the server is taking to handle requests - server created

---
+++ week 3/week-3.1/assignments/2_Find_the_average_time_the_server_istaking_to_handle_requests/index.js
@@ -0,0 +1,14 @@
+const express = require('express')
+
+const app = express()
+const PORT = 5500
+
+app.get('/', (req, res) => {
+	res.json({
+		message: "Welcome to Anoop's Server.",
+	})
+})
+
+app.listen(PORT, () => {
+	console.log(`Server started at port ${PORT}.`)
+})
