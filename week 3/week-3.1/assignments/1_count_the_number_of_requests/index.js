c3190ebf8fef3c0c021d75446723aa2c6fc3cefd
anoopraju31 <anoopraju31@gmail.com>
2023-12-17 08:28:00 +0530

    Week 3.1 assignment: 1 Count the number of requests - middleware for counting requests and route to get it are added

--- week 3/week-3.1/assignments/1_count_the_number_of_requests/index.js
+++ week 3/week-3.1/assignments/1_count_the_number_of_requests/index.js
@@ -2,10 +2,24 @@ const express = require('express')
 
 const app = express()
 const PORT = 5500
+let numberOfRequests = 0
+
+function countRequestsMiddleware(req, res, next) {
+	numberOfRequests++
+	next()
+}
+
+app.use(countRequestsMiddleware)
 
 app.get('/', (req, res) => {
 	res.json({
 		message: "Welcome to Anoop's server.",
+	})
+})
+
+app.get('/number-of-requests', (req, res) => {
+	res.json({
+		numberOfRequests,
 	})
 })
 
