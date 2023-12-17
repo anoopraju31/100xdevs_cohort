f7c4f1984c0a9c3b79e47bc98ed6393dfcbdbe84
anoopraju31 <anoopraju31@gmail.com>
2023-12-17 08:51:32 +0530

    Week 3.1 Assignment: Calculate the average time the server is taking to handle requests - middleware to calculate time taken for a request and route to get average time taken for a request is added

--- week 3/week-3.1/assignments/2_Find_the_average_time_the_server_istaking_to_handle_requests/index.js
+++ week 3/week-3.1/assignments/2_Find_the_average_time_the_server_istaking_to_handle_requests/index.js
@@ -3,9 +3,42 @@ const express = require('express')
 const app = express()
 const PORT = 5500
 
+const timeTakenToHandleRequests = []
+
+function requestTimeMiddleware(req, res, next) {
+	const startTime = Date.now()
+
+	res.on('finish', () => {
+		const endTime = Date.now()
+		const duration = endTime - startTime
+
+		console.log(duration)
+
+		timeTakenToHandleRequests.push(duration)
+	})
+
+	next()
+}
+
+app.use(requestTimeMiddleware)
+
 app.get('/', (req, res) => {
 	res.json({
 		message: "Welcome to Anoop's Server.",
+	})
+})
+
+app.get('/average-time-to-handle-requests', (req, res) => {
+	const totalTimeTaken = timeTakenToHandleRequests.reduce(
+		(acc, val) => acc + val,
+		0,
+	)
+	const numberOfRequests = timeTakenToHandleRequests.length
+	const averageTimeTakenToHandleRequests = totalTimeTaken / numberOfRequests
+
+	res.json({
+		numberOfRequests,
+		averageTimeTakenToHandleRequests: `${averageTimeTakenToHandleRequests} ms`,
 	})
 })
 
