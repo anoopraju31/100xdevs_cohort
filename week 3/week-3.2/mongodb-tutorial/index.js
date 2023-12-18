faf16703358d239c92a16229889232f0e2479839
anoopraju31 <anoopraju31@gmail.com>
2023-12-18 23:56:33 +0530

    Week 3.2 - mongodb-tutorial: sign route initial

--- week 3/week-3.2/mongodb-tutorial/index.js
+++ week 3/week-3.2/mongodb-tutorial/index.js
@@ -3,9 +3,21 @@ const express = require('express')
 const app = express()
 const PORT = 5500
 
+app.use(express.json())
+
 app.get('/', (req, res) => {
 	res.json({
 		message: "Anoop's server started!",
+	})
+})
+
+app.post('/signin', (req, res) => {
+	const username = req.body.username
+	const password = req.body.password
+
+	res.json({
+		username,
+		password,
 	})
 })
 
