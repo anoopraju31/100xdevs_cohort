7248b9b58363fc1318a93299460db863dd75babb
anoopraju31 <anoopraju31@gmail.com>
2023-12-19 13:35:12 +0530

    Week 3 Assignment: jwt

--- week 3/assigments/02-jwt/index.js
+++ week 3/assigments/02-jwt/index.js
@@ -1,21 +1,40 @@
-const jwt = require('jsonwebtoken');
-const jwtPassword = "secret";
+const jwt = require('jsonwebtoken')
+const zod = require('zod')
+const jwtPassword = 'secret'
 
 function signJwt(username, password) {
+	const usernameSchema = zod.string().min(1).email()
+	const passwordSchema = zod.string().min(6)
 
+	const isValidUsername = usernameSchema.safeParse(username)
+	const isValidPassword = passwordSchema.safeParse(password)
+
+	if (!isValidUsername.success || !isValidPassword.success) return null
+
+	const encode = jwt.sign({ username }, jwtPassword)
+
+	return encode
 }
 
 function verifyJwt(token) {
+	try {
+		jwt.verify(token, jwtPassword)
 
+		return true
+	} catch (error) {
+		return false
+	}
 }
 
 function decodeJwt(token) {
+	const decoded = jwt.decode(token)
 
+	return decoded !== null
 }
 
 module.exports = {
-    signJwt,
-    verifyJwt,
-    decodeJwt,
-    jwtPassword
+	signJwt,
+	verifyJwt,
+	decodeJwt,
+	jwtPassword,
 }
