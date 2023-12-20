23d009afc63114f6d1ac8dfa5eda4ac37ed7c4dd
anoopraju31 <anoopraju31@gmail.com>
2023-12-20 23:35:33 +0530

    Week 3 Assignment - 04-mongo-with-jwt-auth: User - user middleware added

--- week 3/assigments/04-mongo-with-jwt-auth/middleware/user.js
+++ week 3/assigments/04-mongo-with-jwt-auth/middleware/user.js
@@ -1,6 +1,19 @@
 function userMiddleware(req, res, next) {
-    // Implement user auth logic
-    // You need to check the headers and validate the user from the user DB. Check readme for the exact headers to be expected
+	try {
+		const authorization = req.headers?.authorization
+		const secret = process.env.JWT_SECRET
+		const token = authorization.split(' ')[1]
+
+		const decoded = jwt.verify(token, secret)
+
+		req.headers['user-id'] = decoded.id
+
+		next()
+	} catch (error) {
+		console.error(error)
+
+		res.status(403).json({ message: 'unauthorized access' })
+	}
 }
 
-module.exports = userMiddleware;
+module.exports = userMiddleware
