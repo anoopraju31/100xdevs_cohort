921c06b2f5abc84526c8e9c20c9d3c3cebd89b1f
anoopraju31 <anoopraju31@gmail.com>
2023-12-20 23:22:25 +0530

    Week 3 Assignment - 04-mongo-with-jwt-auth: User - signup route added

--- week 3/assigments/04-mongo-with-jwt-auth/routes/user.js
+++ week 3/assigments/04-mongo-with-jwt-auth/routes/user.js
@@ -1,24 +1,66 @@
-const { Router } = require("express");
-const router = Router();
-const userMiddleware = require("../middleware/user");
+const { Router } = require('express')
+const zod = require('zod')
+const userMiddleware = require('../middleware/user')
+const { userExists } = require('../utills')
+const { User } = require('../db')
+
+const router = Router()
+const credentialSchema = zod.object({
+	email: zod.string().email(),
+	password: zod.string().min(8),
+})
 
 // User Routes
-app.post('/signup', (req, res) => {
-    // Implement user signup logic
-});
+router.post('/signup', async (req, res) => {
+	try {
+		const username = req.body.username
+		const password = req.body.password
+		const isValidCredentials = credentialSchema.safeParse({
+			username,
+			password,
+		})
 
-app.post('/signin', (req, res) => {
-    // Implement admin signup logic
-});
+		if (!isValidCredentials)
+			return res.status(400).json({
+				message: 'Invalid Credentials',
+			})
 
-app.get('/courses', (req, res) => {
-    // Implement listing all courses logic
-});
+		const isUserExists = await userExists(username)
 
-app.post('/courses/:courseId', userMiddleware, (req, res) => {
-    // Implement course purchase logic
-});
+		if (isUserExists)
+			return res.status(400).json({
+				message: 'username does not exists',
+			})
 
-app.get('/purchasedCourses', userMiddleware, (req, res) => {
-    // Implement fetching purchased courses logic
-});
+		const user = new User({
+			username,
+			password,
+		})
+
+		await user.save()
+
+		res.json({ message: 'User created successfully' })
+	} catch (error) {
+		console.error(error)
+
+		res.status(500).json({ message: 'something went wrong' })
+	}
+})
+
+// app.post('/signin', (req, res) => {
+// 	// Implement admin signup logic
+// })
+
+// app.get('/courses', (req, res) => {
+// 	// Implement listing all courses logic
+// })
+
+// app.post('/courses/:courseId', userMiddleware, (req, res) => {
+// 	// Implement course purchase logic
+// })
+
+// app.get('/purchasedCourses', userMiddleware, (req, res) => {
+// 	// Implement fetching purchased courses logic
+// })
+
+module.exports = router
--- week 3/assigments/04-mongo-with-jwt-auth/utills/index.js
+++ week 3/assigments/04-mongo-with-jwt-auth/utills/index.js
@@ -1,4 +1,4 @@
-const { Admin } = require('../db')
+const { Admin, User } = require('../db')
 
 async function adminExists(username) {
 	const existingUser = await Admin.findOne({ username })
@@ -6,6 +6,13 @@ async function adminExists(username) {
 	return existingUser
 }
 
+async function userExists(username) {
+	const existingUser = await User.findOne({ username })
+
+	return existingUser
+}
+
 module.exports = {
 	adminExists,
+	userExists,
 }
