7ef7b0439bd4e303985e599a98fb2157ffb72f75
anoopraju31 <anoopraju31@gmail.com>
2023-12-20 21:36:31 +0530

    Week 3 Assignment - 04-mongo-with-jwt-auth: mongodb connection added

--- week 3/assigments/04-mongo-with-jwt-auth/db/index.js
+++ week 3/assigments/04-mongo-with-jwt-auth/db/index.js
@@ -1,27 +1,36 @@
-const mongoose = require('mongoose');
+const mongoose = require('mongoose')
+require('dotenv').config()
 
 // Connect to MongoDB
-mongoose.connect('your-mongodb-url');
+mongoose.connect(process.env.MONGODB_CONNECTION_URL)
 
 // Define schemas
 const AdminSchema = new mongoose.Schema({
-    // Schema definition here
-});
+	username: String,
+	password: String,
+	courses: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Course' }],
+})
 
 const UserSchema = new mongoose.Schema({
-    // Schema definition here
-});
+	username: String,
+	password: String,
+	courses: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Course' }],
+})
 
 const CourseSchema = new mongoose.Schema({
-    // Schema definition here
-});
+	title: String,
+	description: String,
+	price: Number,
+	imageLink: String,
+	author: { type: mongoose.Schema.Types.ObjectId, ref: 'Course' },
+})
 
-const Admin = mongoose.model('Admin', AdminSchema);
-const User = mongoose.model('User', UserSchema);
-const Course = mongoose.model('Course', CourseSchema);
+const Admin = mongoose.model('Admin', AdminSchema)
+const User = mongoose.model('User', UserSchema)
+const Course = mongoose.model('Course', CourseSchema)
 
 module.exports = {
-    Admin,
-    User,
-    Course
+	Admin,
+	User,
+	Course,
 }
