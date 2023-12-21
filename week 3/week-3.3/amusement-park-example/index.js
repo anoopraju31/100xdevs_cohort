23b9ab5ac38bdad29276d712d761c73c5afb79b3
anoopraju31 <anoopraju31@gmail.com>
2023-12-21 11:55:29 +0530

    Week 3.3 - amusement park example - ride age limit added

--- week 3/week-3.3/amusement-park-example/index.js
+++ week 3/week-3.3/amusement-park-example/index.js
@@ -3,8 +3,17 @@ const express = require('express')
 const app = express()
 const PORT = 5500
 
+// * function that returns a boolean if the age of the person is more than 14.
+function isOldEnough(age) {
+	return age >= 14
+}
+
 app.get('/ride', (req, res) => {
-	res.json({ message: 'You have successfully riden the ride.' })
+	const age = parseInt(req.query.age)
+
+	if (isOldEnough(age))
+		return res.json({ message: 'You have successfully riden the ride.' })
+	else return res.json({ message: 'Sorry, you are underaged for this ride.' })
 })
 
 app.listen(PORT, () => {
