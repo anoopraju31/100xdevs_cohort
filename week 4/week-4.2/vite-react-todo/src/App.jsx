8ac7fe268ba5b717bfcc771040ec77d7e2a81816
anoopraju31 <anoopraju31@gmail.com>
2023-12-27 17:10:36 +0530

    App style refactor

--- week 4/week-4.2/vite-react-todo/src/App.jsx
+++ week 4/week-4.2/vite-react-todo/src/App.jsx
@@ -7,19 +7,19 @@ function App() {
 		<main className='p-10 bg-gray-100 min-h-screen'>
 			<div className='container mx-auto max-w-3xl'>
 				<Header />
+
+				<section className='rounded-lg border bg-card text-card-foreground shadow-sm'>
+					<div className='flex flex-col space-y-1.5 p-6'>
+						<h3 className='font-semibold text-center tracking-tight text-lg'>
+							Add New Task
+						</h3>
+					</div>
+
+					<Form />
+				</section>
+
+				<Tasks />
 			</div>
-
-			<section className='rounded-lg border bg-card text-card-foreground shadow-sm'>
-				<div className='flex flex-col space-y-1.5 p-6'>
-					<h3 className='font-semibold text-center tracking-tight text-lg'>
-						Add New Task
-					</h3>
-				</div>
-
-				<Form />
-			</section>
-
-			<Tasks />
 		</main>
 	)
 }
