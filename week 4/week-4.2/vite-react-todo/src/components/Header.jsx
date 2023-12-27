0315ca26a935e72009e59bd54bb96f88784a3448
anoopraju31 <anoopraju31@gmail.com>
2023-12-27 16:27:36 +0530

    Week 4.2 React - Vite - Header Component - styled

--- week 4/week-4.2/vite-react-todo/src/components/Header.jsx
+++ week 4/week-4.2/vite-react-todo/src/components/Header.jsx
@@ -1,5 +1,9 @@
 const Header = () => {
-	return <div>Header</div>
+	return (
+		<header className='flex h-20 w-full items-center justify-center px-6 bg-gray-100'>
+			<h1 className='text-2xl font-bold'>Todo Application</h1>
+		</header>
+	)
 }
 
 export default Header
