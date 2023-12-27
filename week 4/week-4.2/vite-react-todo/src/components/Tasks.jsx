0788583f069008611a671c84fd2512bbd4264007
anoopraju31 <anoopraju31@gmail.com>
2023-12-27 17:23:01 +0530

    Week 4.2 React - Vite - Tasks Component - props added

--- week 4/week-4.2/vite-react-todo/src/components/Tasks.jsx
+++ week 4/week-4.2/vite-react-todo/src/components/Tasks.jsx
@@ -1,11 +1,19 @@
+import PropTypes from 'prop-types'
 import Task from './Task'
 
-const Tasks = () => {
+const Tasks = (props) => {
+	const { tasks } = props
 	return (
 		<section className='grid gap-6 mt-4'>
-			<Task />
+			{tasks.map((task) => (
+				<Task key={task.id} />
+			))}
 		</section>
 	)
+}
+
+Tasks.propTypes = {
+	tasks: PropTypes.array.isRequired,
 }
 
 export default Tasks
