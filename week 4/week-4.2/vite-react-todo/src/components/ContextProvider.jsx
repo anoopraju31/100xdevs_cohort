c5cd228b45cfb0aa3c90d5c57a311308cda9f50f
anoopraju31 <anoopraju31@gmail.com>
2023-12-27 18:05:56 +0530

    Week 4.2 React - Vite - Context API - create task functionality added

--- week 4/week-4.2/vite-react-todo/src/components/ContextProvider.jsx
+++ week 4/week-4.2/vite-react-todo/src/components/ContextProvider.jsx
@@ -1,11 +1,30 @@
 import { createContext, useState } from 'react'
 import PropTypes from 'prop-types'
+import { v4 as uuidv4 } from 'uuid'
 
 export const AppContext = createContext()
 const ContextProvider = (props) => {
 	const { children } = props
 	const [tasks, setTasks] = useState([])
+	const [task, setTask] = useState({
+		title: '',
+		description: '',
+		completed: false,
+		id: '',
+	})
 
+	const handleTitleChange = (e) =>
+		setTask((prev) => ({ ...prev, title: e.target.value }))
+	const handleDescriptionChange = (e) =>
+		setTask((prev) => ({ ...prev, description: e.target.value }))
+	const addTaskId = () => setTask((prev) => ({ ...prev, id: uuidv4() }))
+	const clearTask = () =>
+		setTask({
+			title: '',
+			description: '',
+			completed: false,
+			id: '',
+		})
 	const handleTasks = (task) => setTasks((prev) => [...prev, task])
 	const deleteTask = (id) => {
 		setTasks((prev) => prev.filter((task) => task.id !== id))
@@ -26,6 +45,11 @@ const ContextProvider = (props) => {
 		handleTasks,
 		deleteTask,
 		toggleCompletion,
+		task,
+		handleTitleChange,
+		handleDescriptionChange,
+		addTaskId,
+		clearTask,
 	}
 	return <AppContext.Provider value={data}>{children}</AppContext.Provider>
 }
