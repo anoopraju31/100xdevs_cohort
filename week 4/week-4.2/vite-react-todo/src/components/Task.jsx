f326ef0815f174ef6a82bd4c40a1578455fcca33
anoopraju31 <anoopraju31@gmail.com>
2023-12-27 17:04:12 +0530

    Week 4.2 React - Vite - Task Component - styled

--- week 4/week-4.2/vite-react-todo/src/components/Task.jsx
+++ week 4/week-4.2/vite-react-todo/src/components/Task.jsx
@@ -1,5 +1,23 @@
+import Button from './Button'
+
 const Task = () => {
-	return <div>Task</div>
+	return (
+		<div className='rounded-lg border bg-card text-card-foreground shadow-sm'>
+			<div className='flex flex-col space-y-1.5 p-6'>
+				<h3 className='text-2xl font-semibold leading-none tracking-tight'>
+					Title
+				</h3>
+			</div>
+
+			<p className='p-6 text-sm'> description </p>
+
+			<div className='items-center p-6 flex gap-4 justify-end'>
+				<Button title='Mark as done' />
+				<Button title='Edit' />
+				<Button title='Delete' />
+			</div>
+		</div>
+	)
 }
 
 export default Task
