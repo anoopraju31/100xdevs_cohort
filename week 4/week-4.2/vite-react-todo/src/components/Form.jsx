cb36339b52bb73f4d7d82badcd22266af6e2874e
anoopraju31 <anoopraju31@gmail.com>
2023-12-27 16:49:19 +0530

    Week 4.2 React - Vite - Form Field Component - styled

--- week 4/week-4.2/vite-react-todo/src/components/Form.jsx
+++ week 4/week-4.2/vite-react-todo/src/components/Form.jsx
@@ -1,9 +1,31 @@
+import { useState } from 'react'
 import FormField from './FormField'
 
 const Form = () => {
+	const [title, setTitle] = useState('')
+	const [description, setDescription] = useState('')
+
+	const handleTitleChange = (e) => setTitle(e.target.value)
+	const handleDescriptionChange = (e) => setDescription(e.target.value)
 	return (
 		<form className='p-6 flex flex-col gap-2'>
-			<FormField />
+			<FormField
+				id='title'
+				label='Title'
+				placeholder='Enter title'
+				type='text'
+				value={title}
+				handleChange={handleTitleChange}
+			/>
+
+			<FormField
+				id='description'
+				label='Description'
+				placeholder='Enter description'
+				type='text'
+				value={description}
+				handleChange={handleDescriptionChange}
+			/>
 		</form>
 	)
 }
--- week 4/week-4.2/vite-react-todo/src/components/FormField.jsx
+++ week 4/week-4.2/vite-react-todo/src/components/FormField.jsx
@@ -1,5 +1,30 @@
-const FormField = () => {
-	return <div>FormField</div>
+import PropTypes from 'prop-types'
+const FormField = (props) => {
+	const { id, label, placeholder, value, handleChange, type } = props
+	return (
+		<div>
+			<label className='sr-only' htmlFor={id}>
+				{label}
+			</label>
+			<input
+				id={id}
+				value={value}
+				className='flex h-10 rounded-md border border-input bg-background px-3 py-2 text-sm ring-offset-background file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50 flex-grow w-full'
+				placeholder={placeholder}
+				type={type}
+				onChange={handleChange}
+			/>
+		</div>
+	)
+}
+
+FormField.propTypes = {
+	id: PropTypes.string.isRequired,
+	label: PropTypes.string.isRequired,
+	placeholder: PropTypes.string.isRequired,
+	value: PropTypes.string.isRequired,
+	handleChange: PropTypes.func.isRequired,
+	type: PropTypes.string.isRequired,
 }
 
 export default FormField
