589d691cb6794cf28b80aea67d59c0781d1c9458
anoopraju31 <anoopraju31@gmail.com>
2023-12-27 18:28:34 +0530

    Week 4.2 React - Vite - Context API - refactor

--- week 4/week-4.2/vite-react-todo/src/components/ContextProvider.jsx
+++ week 4/week-4.2/vite-react-todo/src/components/ContextProvider.jsx
@@ -6,6 +6,7 @@ export const AppContext = createContext()
 const ContextProvider = (props) => {
 	const { children } = props
 	const [tasks, setTasks] = useState([])
+	const [isEditForm, setIsEditForm] = useState(false)
 	const [task, setTask] = useState({
 		title: '',
 		description: '',
@@ -15,9 +16,10 @@ const ContextProvider = (props) => {
 
 	const handleTitleChange = (e) =>
 		setTask((prev) => ({ ...prev, title: e.target.value }))
+
 	const handleDescriptionChange = (e) =>
 		setTask((prev) => ({ ...prev, description: e.target.value }))
-	const addTaskId = () => setTask((prev) => ({ ...prev, id: uuidv4() }))
+
 	const clearTask = () =>
 		setTask({
 			title: '',
@@ -25,10 +27,14 @@ const ContextProvider = (props) => {
 			completed: false,
 			id: '',
 		})
-	const handleTasks = (task) => setTasks((prev) => [...prev, task])
+
+	const handleTasks = (task) =>
+		setTasks((prev) => [...prev, { ...task, id: uuidv4() }])
+
 	const deleteTask = (id) => {
 		setTasks((prev) => prev.filter((task) => task.id !== id))
 	}
+
 	const toggleCompletion = (id) => {
 		const alterTasks = tasks.map((task) => {
 			if (task.id === id) {
@@ -39,12 +45,22 @@ const ContextProvider = (props) => {
 		})
 		setTasks(alterTasks)
 	}
+
 	const editTask = (id) => {
 		const filteredTasks = tasks.filter((task) => task.id === id)
 
 		if (!filteredTasks) return
 
 		setTask(filteredTasks[0])
+		setIsEditForm(true)
+	}
+
+	const handleEditTask = (editedTask) => {
+		const alteredTasks = tasks.map((task) =>
+			task.id === editedTask.id ? editedTask : task,
+		)
+
+		setTasks(alteredTasks)
 	}
 
 	const data = {
@@ -55,9 +71,10 @@ const ContextProvider = (props) => {
 		task,
 		handleTitleChange,
 		handleDescriptionChange,
-		addTaskId,
 		clearTask,
 		editTask,
+		isEditForm,
+		handleEditTask,
 	}
 	return <AppContext.Provider value={data}>{children}</AppContext.Provider>
 }
