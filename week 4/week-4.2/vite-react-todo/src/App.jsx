26b5a4d7468f5d171821a534277cb66dd715ef25
anoopraju31 <anoopraju31@gmail.com>
2023-12-27 16:27:12 +0530

    Week 4.2 React - Vite - Header Component - initial

--- week 4/week-4.2/vite-react-todo/src/App.jsx
+++ week 4/week-4.2/vite-react-todo/src/App.jsx
@@ -1,8 +1,12 @@
+import Header from './components/Header'
+
 function App() {
 	return (
-		<>
-			<h1 className='text-3xl font-bold underline'>Hello world!</h1>
-		</>
+		<main className='p-10 bg-gray-100 min-h-screen'>
+			<div className='container mx-auto max-w-3xl'>
+				<Header />
+			</div>
+		</main>
 	)
 }
 
