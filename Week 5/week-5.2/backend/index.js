f44e85e263b40df9d3c56dca9ebf6ed8d191f4d9
anoopraju31 <anoopraju31@gmail.com>
2023-12-31 19:36:53 +0530

    Week 5.2 - Todo backend: validation added to create todo route

--- Week 5/week-5.2/backend/index.js
+++ Week 5/week-5.2/backend/index.js
@@ -1,4 +1,7 @@
 const express = require('express')
+const { createTodo } = require('./type')
+require('dotenv').config()
+
 const app = express()
 const PORT = 5500
 
@@ -6,7 +9,13 @@ app.use(express.json())
 
 app.get('/todo', (req, res) => {})
 
-app.post('/todo', (req, res) => {})
+app.post('/todo', (req, res) => {
+	const { title, description } = req.body
+	const validate = createTodo.safeParse({ title, description })
+
+	if (!validate.success)
+		return res.status(411).json({ message: 'You sent the wrong inputs' })
+})
 
 app.put('/completed', (req, res) => {})
 
