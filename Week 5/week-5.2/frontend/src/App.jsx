e27456424bc87198190999401d3ed33823aa89c2
anoopraju31 <anoopraju31@gmail.com>
2023-12-31 20:54:03 +0530

    Week 5.2 - Todo frontend: fetching todos and displaying it

--- Week 5/week-5.2/frontend/src/App.jsx
+++ Week 5/week-5.2/frontend/src/App.jsx
@@ -1,11 +1,25 @@
-import CreateTodo from './components/CreateTodo'
+import { useEffect, useState } from 'react'
+import { CreateTodo } from './components/CreateTodo'
 import { Todos } from './components/Todos'
 
 const App = () => {
+	const [todos, setTodos] = useState([])
+
+	useEffect(() => {
+		const fetchData = async () => {
+			const res = await fetch('http://localhost:5500/todo')
+			const data = await res.json()
+
+			setTodos(data)
+		}
+
+		fetchData()
+	}, [])
+
 	return (
 		<div>
 			<CreateTodo />
-			<Todos />
+			<Todos todos={todos} />
 		</div>
 	)
 }
