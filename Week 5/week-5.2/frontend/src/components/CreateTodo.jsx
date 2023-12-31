5e3db0414c901a67f016417383348126643a8c88
anoopraju31 <anoopraju31@gmail.com>
2023-12-31 20:54:21 +0530

    Week 5.2 - Todo frontend: creating todo

--- Week 5/week-5.2/frontend/src/components/CreateTodo.jsx
+++ Week 5/week-5.2/frontend/src/components/CreateTodo.jsx
@@ -1,31 +1,61 @@
-const CreateTodo = () => {
+import { useState } from 'react'
+
+export function CreateTodo(props) {
+	// react-query
+	const [title, setTitle] = useState('')
+	const [description, setDescription] = useState('')
+
 	return (
 		<div>
 			<input
+				id='title'
 				style={{
 					padding: 10,
 					margin: 10,
 				}}
 				type='text'
-				placeholder='title'></input>{' '}
+				placeholder='title'
+				onChange={function (e) {
+					const value = e.target.value
+					setTitle(e.target.value)
+				}}></input>{' '}
 			<br />
 			<input
+				id='desc'
 				style={{
 					padding: 10,
 					margin: 10,
 				}}
 				type='text'
-				placeholder='description'></input>{' '}
+				placeholder='description'
+				onChange={function (e) {
+					const value = e.target.value
+					setDescription(e.target.value)
+				}}></input>{' '}
 			<br />
 			<button
 				style={{
 					padding: 10,
 					margin: 10,
+				}}
+				onClick={() => {
+					// axios
+					fetch('http://localhost:5500/todo', {
+						method: 'POST',
+						body: JSON.stringify({
+							title: title,
+							description: description,
+						}),
+						headers: {
+							'Content-type': 'application/json',
+						},
+					}).then(async function (res) {
+						const json = await res.json()
+						alert('Todo added')
+					})
 				}}>
 				Add a todo
 			</button>
 		</div>
 	)
 }
-
-export default CreateTodo
