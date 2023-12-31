104ee80dfe675aafb4aea350072eb90786051223
anoopraju31 <anoopraju31@gmail.com>
2023-12-31 19:30:25 +0530

    Week 5.2 - Todo backend: zod validation added

--- Week 5/week-5.2/backend/index.js
+++ Week 5/week-5.2/backend/index.js
@@ -1,2 +1,15 @@
 const express = require('express')
 const app = express()
+const PORT = 5500
+
+app.use(express.json())
+
+app.get('/todo', (req, res) => {})
+
+app.post('/todo', (req, res) => {})
+
+app.put('/completed', (req, res) => {})
+
+app.listen(PORT, () => {
+	console.log(`Server started at port ${PORT}`)
+})
