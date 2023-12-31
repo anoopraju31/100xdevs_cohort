342f45a4766bda6ed57728c7b5806228b2ecc8c9
anoopraju31 <anoopraju31@gmail.com>
2023-12-31 20:19:55 +0530

    Week 5.2 - Todo frontend: todos component added

--- Week 5/week-5.2/frontend/src/App.jsx
+++ Week 5/week-5.2/frontend/src/App.jsx
@@ -1,9 +1,11 @@
 import CreateTodo from './components/CreateTodo'
+import { Todos } from './components/Todos'
 
 const App = () => {
 	return (
 		<div>
 			<CreateTodo />
+			<Todos />
 		</div>
 	)
 }
---
+++ Week 5/week-5.2/frontend/src/components/Todos.jsx
@@ -0,0 +1,9 @@
+export function Todos() {
+	return (
+		<div>
+			<h1>Go to gym</h1>
+			<h2>You need to go to the gym</h2>
+			<button>Mark as completed</button>
+		</div>
+	)
+}
