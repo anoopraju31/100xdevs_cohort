ce43e6bca72719258feec9fec586c13af940a881
anoopraju31 <anoopraju31@gmail.com>
2024-01-08 22:18:14 +0530

    Week 6.1 React Re-render - Header Component added to App

--- week 6/week-6.1/react-re_rendering/src/App.jsx
+++ week 6/week-6.1/react-re_rendering/src/App.jsx
@@ -1,5 +1,21 @@
+import PropTypes from 'prop-types'
+import React from 'react'
+
 const App = () => {
-	return <div>App</div>
+	return (
+		<React.Fragment>
+			<Header title='My name is Anoop Raju.' />
+			<Header title='My name is Hemanth Kumar.' />
+		</React.Fragment>
+	)
+}
+
+const Header = ({ title }) => {
+	return <div> {title} </div>
+}
+
+Header.propTypes = {
+	title: PropTypes.string,
 }
 
 export default App
