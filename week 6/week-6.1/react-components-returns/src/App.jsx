6591d29d1f73ddea0942320229607eeed627a967
anoopraju31 <anoopraju31@gmail.com>
2024-01-08 21:14:46 +0530

    Week 6.1 React Component Return - Header added

--- week 6/week-6.1/react-components-returns/src/App.jsx
+++ week 6/week-6.1/react-components-returns/src/App.jsx
@@ -1,5 +1,20 @@
+import PropTypes from 'prop-types'
+
 const App = () => {
-	return <div>App</div>
+	return (
+		<div>
+			<Header title='My name is Anoop Raju.' />
+			<Header title='My name is Hemanth Kumar.' />
+		</div>
+	)
+}
+
+const Header = ({ title }) => {
+	return <div> {title} </div>
+}
+
+Header.propTypes = {
+	title: PropTypes.string,
 }
 
 export default App
