d708ebf5c2aac98914f7a0cca57430102b17fe37
anoopraju31 <anoopraju31@gmail.com>
2024-01-08 22:54:16 +0530

    Week 6.1 - React Re-rendering: App updated

--- week 6/week-6.1/react-re_rendering/src/App.jsx
+++ week 6/week-6.1/react-re_rendering/src/App.jsx
@@ -2,16 +2,9 @@ import { Fragment, useState } from 'react'
 import PropTypes from 'prop-types'
 
 const App = () => {
-	const [title, setTitle] = useState('My name is Anoop Raju.')
-
-	const changeTitle = () => {
-		setTitle(`My Name is ${Math.random()}`)
-	}
-
 	return (
 		<Fragment>
-			<button onClick={changeTitle}> update title </button>
-			<Header title={title} />
+			<HeaderWithButton />
 			<Header title='My name is Hemanth Kumar.' />
 		</Fragment>
 	)
@@ -19,6 +12,21 @@ const App = () => {
 
 const Header = ({ title }) => {
 	return <div> {title} </div>
+}
+
+const HeaderWithButton = () => {
+	const [title, setTitle] = useState('My name is Anoop Raju.')
+
+	const changeTitle = () => {
+		setTitle(`My Name is ${Math.random()}`)
+	}
+
+	return (
+		<>
+			<button onClick={changeTitle}> update title </button>
+			<Header title={title} />
+		</>
+	)
 }
 
 Header.propTypes = {
