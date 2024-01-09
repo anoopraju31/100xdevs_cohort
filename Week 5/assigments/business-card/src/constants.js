73f04de3b1e9959bd4ee5c2c07fa12b0e5a23a6a
anoopraju31 <anoopraju31@gmail.com>
2024-01-09 16:39:12 +0530

    Week 5 - Assignments: Business Card: user data moved to contants file

---
+++ Week 5/assigments/business-card/src/constants.js
@@ -0,0 +1,35 @@
+export const user = {
+	name: 'Anoop Raju',
+	description: 'A Computer Science Engineering graduate',
+	interests: [
+		{
+			id: '1',
+			interest: 'Coding',
+		},
+		{
+			id: '2',
+			interest: 'Photography',
+		},
+		{
+			id: '3',
+			interest: 'Travel',
+		},
+	],
+	socials: [
+		{
+			id: '1',
+			title: 'LinkedIn',
+			link: 'www.linkedin.com',
+		},
+		{
+			id: '2',
+			title: 'Instagram',
+			link: 'www.instagram.com',
+		},
+		{
+			id: '3',
+			title: 'Github',
+			link: 'www.github.com',
+		},
+	],
+}
