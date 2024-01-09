1bbfc0770937de6c1edf53cba0b259f80ab4be58
anoopraju31 <anoopraju31@gmail.com>
2024-01-09 16:48:27 +0530

    Week 5 - Assignments: Business Card - Form Component - initial

--- Week 5/assigments/business-card/src/pages/CreateCard.jsx
+++ Week 5/assigments/business-card/src/pages/CreateCard.jsx
@@ -1,7 +1,10 @@
+import Form from '../components/Form'
+
 const CreateCard = () => {
 	return (
-		<main className='max-w-xl h-screen mx-auto flex justify-center items-center'>
+		<main className='max-w-xl h-screen mx-auto flex justify-center items-center flex-col gap-10'>
 			<h1 className='text-3xl font-bold font-mono uppercase'> Create Card </h1>
+			<Form />
 		</main>
 	)
 }
