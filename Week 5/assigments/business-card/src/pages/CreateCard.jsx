d6fe80ddb7ca8fcd7164ae1df8cdff8c9f7937be
anoopraju31 <anoopraju31@gmail.com>
2024-01-09 16:47:01 +0530

    Week 5 - Assignments: Business Card - Create Card Page - heading added

--- Week 5/assigments/business-card/src/pages/CreateCard.jsx
+++ Week 5/assigments/business-card/src/pages/CreateCard.jsx
@@ -1,5 +1,9 @@
 const CreateCard = () => {
-	return <div>CreateCard</div>
+	return (
+		<main className='max-w-xl h-screen mx-auto flex justify-center items-center'>
+			<h1 className='text-3xl font-bold font-mono uppercase'> Create Card </h1>
+		</main>
+	)
 }
 
 export default CreateCard
