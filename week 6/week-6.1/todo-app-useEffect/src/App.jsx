cac37fc326ce950fc29ca088e9e267a7a8e8e5ef
anoopraju31 <anoopraju31@gmail.com>
2024-01-09 14:54:52 +0530

    Week 6.1 Todo App UseEffect - Todo Component added

--- week 6/week-6.1/todo-app-useEffect/src/App.jsx
+++ week 6/week-6.1/todo-app-useEffect/src/App.jsx
@@ -1,5 +1,39 @@
+import { useEffect } from 'react'
+import { useState } from 'react'
+import PropType from 'prop-types'
+
 const App = () => {
-	return <div>App</div>
+	const [todos, setTodos] = useState([])
+
+	useEffect(() => {
+		fetch('https://sum-server.100xdevs.com/todos').then(async (res) => {
+			const data = await res.json()
+			setTodos(data.todos)
+		})
+	}, [])
+
+	return (
+		<div>
+			{todos.map(({ id, title, description }) => (
+				<Todo key={id} title={title} description={description} />
+			))}
+		</div>
+	)
+}
+
+const Todo = (props) => {
+	const { title, description } = props
+	return (
+		<div>
+			<h2> {title} </h2>
+			<p> {description} </p>
+		</div>
+	)
+}
+
+Todo.propTypes = {
+	title: PropType.string,
+	description: PropType.string,
 }
 
 export default App
