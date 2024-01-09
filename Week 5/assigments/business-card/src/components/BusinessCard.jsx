7485faa9ff56cec3aeb982c01ae689ec5d04daa4
anoopraju31 <anoopraju31@gmail.com>
2024-01-09 16:24:44 +0530

    Week 5 - Assignments: Business Card: BusinessCard Component - styled

--- Week 5/assigments/business-card/src/components/BusinessCard.jsx
+++ Week 5/assigments/business-card/src/components/BusinessCard.jsx
@@ -3,17 +3,19 @@ import PropTypes from 'prop-types'
 const BusinessCard = (props) => {
 	const { name, description, interests, socials } = props
 	return (
-		<div>
-			<h2> {name} </h2>
-			<p> {description} </p>
-			<div>
-				<h4> Interests </h4>
-				{interests.map(({ id, interest }) => (
-					<p key={id}> {interest} </p>
-				))}
+		<div className='max-w-lg p-10 bg-fuchsia-200 font-mono flex gap-2 justify-center items-center flex-col'>
+			<h2 className='text-4xl mb-4'> {name} </h2>
+			<p className='text-center mb-2'> {description} </p>
+			<div className='flex gap-2 mb-2'>
+				<h4 className='font-medium'> Interests: </h4>
+				<div className='flex items-center gap-2'>
+					{interests.map(({ id, interest }) => (
+						<p key={id}> {interest} </p>
+					))}
+				</div>
 			</div>
 
-			<div className=''>
+			<div className='flex items-center gap-4'>
 				{socials.map(({ id, title, link }) => (
 					<a key={id} href={link}>
 						{' '}
