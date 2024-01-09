73cfe14db22caa30df7a8f911a668d315388296f
anoopraju31 <anoopraju31@gmail.com>
2024-01-09 21:00:30 +0530

    Week 5 - Assignments: Business Card Backend - /business-card route added

--- Week 5/assigments/business-card-backend/index.js
+++ Week 5/assigments/business-card-backend/index.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const businessCardRouter = require('./routes/businessCard.routes')
 
 const app = express()
 const PORT = 5500
@@ -6,6 +7,8 @@ const PORT = 5500
 app.get('/', (req, res) => {
 	res.json({ message: 'Welcome to Business Card App' })
 })
+
+app.use('/business-card', businessCardRouter)
 
 app.listen(PORT, () => {
 	console.log(`Server started at port ${PORT}.`)
