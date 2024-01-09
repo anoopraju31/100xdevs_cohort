82e1bb71c2d042c9c3693256de6c7aedc0861524
anoopraju31 <anoopraju31@gmail.com>
2024-01-10 04:18:22 +0530

    Week 5 - Assigments: Business Card Backend - business card update route added

--- Week 5/assigments/business-card-backend/routes/businessCard.routes.js
+++ Week 5/assigments/business-card-backend/routes/businessCard.routes.js
@@ -49,4 +49,53 @@ router.post('/create', async (req, res) => {
 	}
 })
 
+router.put('/:id', async (req, res) => {
+	try {
+		const id = req.params.id
+
+		const card = await Card.findOne({ _id: id })
+
+		if (!card) {
+			return res.status(401).json({
+				status: 'error',
+				message: 'Card not found',
+			})
+		}
+
+		const { name, description, interests, socials } = req.body
+
+		const validationResponse = cardSchema.safeParse({
+			name,
+			description,
+			interests,
+			socials,
+		})
+
+		if (!validationResponse.success)
+			return res.status(401).json({
+				status: 'error',
+				message: validationResponse.error.issues[0].message,
+			})
+
+		const updatedCard = await Card.updateOne(
+			{ _id: id },
+			{
+				name,
+				description,
+				interests,
+				socials,
+			},
+		)
+
+		res.json({
+			status: 'success',
+			message: 'successfully updated card.',
+		})
+	} catch (error) {
+		console.error(error)
+
+		res.status(500).json({ message: 'something went wrong' })
+	}
+})
+
 module.exports = router
