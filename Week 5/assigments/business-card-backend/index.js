d4ef6661c8e07651f82121f78617d271b8cbceeb
anoopraju31 <anoopraju31@gmail.com>
2024-01-09 21:05:24 +0530

    Week 5 - Assignments: Business Card Backend - MongoDB connection added

--- Week 5/assigments/business-card-backend/index.js
+++ Week 5/assigments/business-card-backend/index.js
@@ -1,8 +1,13 @@
 const express = require('express')
 const businessCardRouter = require('./routes/businessCard.routes')
+const mongoose = require('mongoose')
+require('dotenv').config()
 
 const app = express()
 const PORT = 5500
+const MONGODB_CONNECTION_URL = process.env.MONGODB_CONNECTION_URL
+
+mongoose.connect(MONGODB_CONNECTION_URL)
 
 app.get('/', (req, res) => {
 	res.json({ message: 'Welcome to Business Card App' })
