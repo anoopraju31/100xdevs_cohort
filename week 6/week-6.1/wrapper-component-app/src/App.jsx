bf55c05edd4565190d040dddfa1b575dd593cc38
anoopraju31 <anoopraju31@gmail.com>
2024-01-09 13:11:28 +0530

    Week 6.1 Wrapper Component App - Wrapper and Text Component added

--- week 6/week-6.1/wrapper-component-app/src/App.jsx
+++ week 6/week-6.1/wrapper-component-app/src/App.jsx
@@ -1,5 +1,34 @@
+import PropTypes from 'prop-types'
+
 const App = () => {
-	return <div>App</div>
+	return (
+		<div>
+			<CardWrapper>
+				<TextComponent />
+			</CardWrapper>
+			<TextComponent />
+
+			<CardWrapper>
+				<CardWrapper>
+					<CardWrapper>
+						<TextComponent />
+					</CardWrapper>
+				</CardWrapper>
+			</CardWrapper>
+		</div>
+	)
 }
 
+const TextComponent = () => {
+	return <div className='text-component'> Hi there </div>
+}
+
+const CardWrapper = (props) => {
+	const { children } = props
+	return <div className='wrapper'>{children}</div>
+}
+
+CardWrapper.propTypes = {
+	children: PropTypes.node,
+}
 export default App
