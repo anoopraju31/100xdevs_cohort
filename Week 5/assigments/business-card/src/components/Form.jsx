763b3dbe11173e0a232b75b872ea685030047c37
anoopraju31 <anoopraju31@gmail.com>
2024-01-09 17:30:36 +0530

    Week 5 - Assignments: Business Card - Input Field Component - styled

--- Week 5/assigments/business-card/src/components/Form.jsx
+++ Week 5/assigments/business-card/src/components/Form.jsx
@@ -1,58 +1,41 @@
 import Button from './Button'
+import InputField from './InputField'
 
 const Form = () => {
 	return (
 		<form className='p-10 '>
-			<div className='mb-4'>
-				<label htmlFor='name' className='sr-only'>
-					Name
-				</label>
-				<input
-					type='text'
-					name='name'
-					id='name'
-					placeholder='Enter your name'
-					className='bg-transparent border outline-none border-orange-300 text-orange-900 text-sm rounded-lg focus:ring-orange-500 focus:border-orange-500 block w-full p-2.5 placeholder:text-orange-800/80'
-				/>
-			</div>
+			<InputField
+				id='name'
+				name='name'
+				label='Name'
+				placeholder='Enter your name'
+				type='text'
+			/>
 
-			<div className='mb-4'>
-				<label htmlFor='description' className='sr-only'>
-					Description
-				</label>
-				<textarea
-					rows='5'
-					name='description'
-					id='description'
-					placeholder='Enter your description'
-					className='bg-transparent border outline-none border-orange-300 text-orange-900 text-sm rounded-lg focus:ring-orange-500 focus:border-orange-500 block w-full p-2.5 placeholder:text-orange-800/80'></textarea>
-			</div>
+			<InputField
+				id='description'
+				name='description'
+				label='Description'
+				placeholder='Enter your description'
+				isTextarea
+				rows={5}
+			/>
 
-			<div className='mb-4'>
-				<label htmlFor='interest' className='sr-only'>
-					Interest
-				</label>
-				<input
-					type='text'
-					name='interest'
-					id='interest'
-					placeholder='Enter your interest'
-					className='bg-transparent border outline-none border-orange-300 text-orange-900 text-sm rounded-lg focus:ring-orange-500 focus:border-orange-500 block w-full p-2.5 placeholder:text-orange-800/80'
-				/>
-			</div>
+			<InputField
+				id='interest'
+				name='interest'
+				label='Interest'
+				placeholder='Enter your interest'
+				type='text'
+			/>
 
-			<div className='mb-4'>
-				<label htmlFor='social-link' className='sr-only'>
-					Social
-				</label>
-				<input
-					type='text'
-					name='social-link'
-					id='social-link'
-					placeholder='Enter your social media'
-					className='bg-transparent border outline-none border-orange-300 text-orange-900 text-sm rounded-lg focus:ring-orange-500 focus:border-orange-500 block w-full p-2.5 placeholder:text-orange-800/80'
-				/>
-			</div>
+			<InputField
+				id='social-link'
+				name='social-link'
+				label='Social media link'
+				placeholder='Enter your social media link'
+				type='text'
+			/>
 
 			<div className='mt-6 flex justify-center'>
 				<Button />
--- Week 5/assigments/business-card/src/components/InputField.jsx
+++ Week 5/assigments/business-card/src/components/InputField.jsx
@@ -1,5 +1,41 @@
-const InputField = () => {
-	return <div>InputField</div>
+import PropTypes from 'prop-types'
+
+const InputField = (props) => {
+	const { isTextarea, id, name, label, placeholder, type, rows } = props
+	return (
+		<div className='mb-4'>
+			<label htmlFor={id} className='sr-only'>
+				{label}
+			</label>
+			{isTextarea ? (
+				<textarea
+					rows={rows}
+					name={name}
+					id={id}
+					placeholder={placeholder}
+					className='bg-transparent border outline-none border-orange-300 text-orange-900 text-sm rounded-lg focus:ring-orange-500 focus:border-orange-500 block w-full p-2.5 placeholder:text-orange-800/80'
+				/>
+			) : (
+				<input
+					type={type}
+					name={name}
+					id={id}
+					placeholder={placeholder}
+					className='bg-transparent border outline-none border-orange-300 text-orange-900 text-sm rounded-lg focus:ring-orange-500 focus:border-orange-500 block w-full p-2.5 placeholder:text-orange-800/80'
+				/>
+			)}
+		</div>
+	)
+}
+
+InputField.propTypes = {
+	isTextarea: PropTypes.bool,
+	name: PropTypes.string,
+	label: PropTypes.string,
+	id: PropTypes.string,
+	placeholder: PropTypes.string,
+	type: PropTypes.string,
+	rows: PropTypes.number,
 }
 
 export default InputField
