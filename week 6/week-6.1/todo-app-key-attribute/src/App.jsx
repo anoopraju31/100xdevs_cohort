f786bc8e3e1cd78e16d78951947d09d3bc78d4fc
anoopraju31 <anoopraju31@gmail.com>
2024-01-09 11:46:43 +0530

    Week 6.1 Key attribute: Todo App - App Component - todo added

--- week 6/week-6.1/todo-app-key-attribute/src/App.jsx
+++ week 6/week-6.1/todo-app-key-attribute/src/App.jsx
@@ -1,5 +1,35 @@
+import { useState } from 'react'
+
 const App = () => {
-	return <div>App</div>
+	const [todos, setTodos] = useState([
+		{ id: 1, title: 'Go to GYM', description: 'Go to GYM today' },
+		{ id: 2, title: 'Go to Market', description: 'Go to Market today' },
+		{
+			id: 3,
+			title: 'Go to Hospital',
+			description: 'Go to Hospital tomorrow.',
+		},
+	])
+
+	const addTodo = () => {
+		setTodos((prev) => [
+			...prev,
+			{ id: prev.length, title: 'new todo', description: 'a new todo added' },
+		])
+	}
+
+	return (
+		<div>
+			<button onClick={addTodo}> Add Todo </button>
+
+			{todos.map(({ id, title, description }) => (
+				<div key={id} className=''>
+					<h3> {title} </h3>
+					<p> {description} </p>
+				</div>
+			))}
+		</div>
+	)
 }
 
 export default App
