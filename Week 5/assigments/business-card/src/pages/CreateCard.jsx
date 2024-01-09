2656a01f4c679ea5da0f0a7229f9f70e540e4372
anoopraju31 <anoopraju31@gmail.com>
2024-01-09 17:55:56 +0530

    Week 5 - Assignments: Business Card - Create Card Page - style change

--- Week 5/assigments/business-card/src/pages/CreateCard.jsx
+++ Week 5/assigments/business-card/src/pages/CreateCard.jsx
@@ -2,11 +2,10 @@ import Form from '../components/Form'
 
 const CreateCard = () => {
 	return (
-		<main className='h-screen flex justify-center items-center bg-orange-50'>
+		<main className='min-h-screen flex justify-center items-center bg-orange-50'>
 			<div className='flex-shrink-0 w-full max-w-2xl p-10 rounded-xl bg-orange-100'>
 				<h1 className='text-3xl text-center mb-2 font-bold font-mono text-orange-900'>
-					{' '}
-					Create Card{' '}
+					Create Card
 				</h1>
 				<Form />
 			</div>
