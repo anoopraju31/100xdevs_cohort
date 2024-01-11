63037d2a71698b5f4ea25f7241ea178f03f01826
anoopraju31 <anoopraju31@gmail.com>
2024-01-11 11:06:42 +0530

    Week 5 - Assignment: Business Card Backend - sign in controller added

--- Week 5/assigments/business-card-backend/controllers/user.controllers.js
+++ Week 5/assigments/business-card-backend/controllers/user.controllers.js
@@ -1,9 +1,11 @@
-const { signUpSchema } = require('../utills/validations.js')
+const { signUpSchema, signInSchema } = require('../utills/validations.js')
 const User = require('../models/user.models.js')
 const bcrypt = require('bcrypt')
+const jwt = require('jsonwebtoken')
 require('dotenv').config()
 
 const salt = parseInt(process.env.SALT_ROUNDS)
+const secret = process.env.JWT_SECRET
 
 const signUpController = async (req, res) => {
 	try {
@@ -35,6 +37,42 @@ const signUpController = async (req, res) => {
 	}
 }
 
+const signInController = async (req, res) => {
+	try {
+		const { email, password } = req.body
+		const validationResponse = signInSchema.safeParse({ email, password })
+
+		if (!validationResponse.success)
+			return res.status(401).json(validationResponse.error.issues[0].message)
+
+		const user = await User.findOne({ email }).select('+password')
+
+		if (!user) return res.status(400).json({ message: 'User not found.' })
+
+		const passwordMatch = await bcrypt.compare(password, user?.password)
+
+		if (!passwordMatch)
+			return res.status(403).json({ message: 'Invalid password' })
+
+		const token = jwt.sign(
+			{
+				name: user.name,
+				email,
+				id: user._id.toString(),
+			},
+			secret,
+			{ expiresIn: '30d' },
+		)
+
+		res.json({ message: 'successfully signed in.', token })
+	} catch (error) {
+		console.error(error)
+
+		res.status(500).json({ message: 'something went wrong' })
+	}
+}
+
 module.exports = {
 	signUpController,
+	signInController,
 }
--- Week 5/assigments/business-card-backend/routes/user.routes.js
+++ Week 5/assigments/business-card-backend/routes/user.routes.js
@@ -1,48 +1,12 @@
 const express = require('express')
-const { signInSchema } = require('../utills/validations.js')
-const User = require('../models/user.models.js')
-const bcrypt = require('bcrypt')
-const jwt = require('jsonwebtoken')
-const { signUpController } = require('../controllers/user.controllers.js')
-require('dotenv').config()
+const {
+	signUpController,
+	signInController,
+} = require('../controllers/user.controllers.js')
 
 const router = express.Router()
-const secret = process.env.JWT_SECRET
 
 router.post('/sign-up', signUpController)
-router.post('/sign-in', async (req, res) => {
-	try {
-		const { email, password } = req.body
-		const validationResponse = signInSchema.safeParse({ email, password })
-
-		if (!validationResponse.success)
-			return res.status(401).json(validationResponse.error.issues[0].message)
-
-		const user = await User.findOne({ email }).select('+password')
-
-		if (!user) return res.status(400).json({ message: 'User not found.' })
-
-		const passwordMatch = await bcrypt.compare(password, user?.password)
-
-		if (!passwordMatch)
-			return res.status(403).json({ message: 'Invalid password' })
-
-		const token = jwt.sign(
-			{
-				name: user.name,
-				email,
-				id: user._id.toString(),
-			},
-			secret,
-			{ expiresIn: '30d' },
-		)
-
-		res.json({ message: 'successfully signed in.', token })
-	} catch (error) {
-		console.error(error)
-
-		res.status(500).json({ message: 'something went wrong' })
-	}
-})
+router.post('/sign-in', signInController)
 
 module.exports = router
