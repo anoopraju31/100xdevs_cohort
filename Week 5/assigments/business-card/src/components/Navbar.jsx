ee788e311179f26e8d917fd8347a3092a6a06824
anoopraju31 <anoopraju31@gmail.com>
2024-01-11 14:49:48 +0530

    Week 5 - Assignment: Business Card - Navbar Component - style change

--- Week 5/assigments/business-card/src/components/Navbar.jsx
+++ Week 5/assigments/business-card/src/components/Navbar.jsx
@@ -5,7 +5,7 @@ const Navbar = () => {
 
 	return (
 		<header className='fixed top-0 left-0 w-full z-50'>
-			<div className='max-w-7xl mx-auto py-5 px-5 xl:px-0 flex justify-between items-center'>
+			<div className='max-w-7xl mx-auto py-5 px-5 xl:px-0 flex justify-between items-center backdrop-blur-3xl'>
 				<Link
 					to='/'
 					className='text-xl text-center mb-2 font-bold font-mono text-orange-900'>
