b1df1c9bb02b1c5326e06122a0555ef358889f51
anoopraju31 <anoopraju31@gmail.com>
2024-01-11 12:26:18 +0530

    Week 5 - Assignment: Business Card - Sign Up page style

--- Week 5/assigments/business-card/src/pages/SignUpPage.jsx
+++ Week 5/assigments/business-card/src/pages/SignUpPage.jsx
@@ -1,5 +1,13 @@
 const SignUpPage = () => {
-	return <div>SignUpPage</div>
+	return (
+		<main className='min-h-screen flex justify-center items-center bg-orange-50'>
+			<div className='flex-shrink-0 w-full max-w-2xl p-10 rounded-xl bg-orange-100'>
+				<h1 className='text-3xl text-center mb-2 font-bold font-mono text-orange-900'>
+					Sign Up
+				</h1>
+			</div>
+		</main>
+	)
 }
 
 export default SignUpPage
