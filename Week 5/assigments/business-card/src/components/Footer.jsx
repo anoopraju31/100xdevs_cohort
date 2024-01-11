c9c83e01231d231aff4692952197081c2177eea4
anoopraju31 <anoopraju31@gmail.com>
2024-01-11 15:15:01 +0530

    Week 5 - Assignment: Business Card - Footer Component - styled

--- Week 5/assigments/business-card/src/components/Footer.jsx
+++ Week 5/assigments/business-card/src/components/Footer.jsx
@@ -1,5 +1,18 @@
 const Footer = () => {
-	return <div>Footer</div>
+	return (
+		<footer className='pb-10 bg-orange-50 text-center'>
+			<p className='text-lg text-center mb-2 font-bold font-mono text-orange-900'>
+				Made by{' '}
+				<a
+					className='underline hover:line-through transition-all duration-300'
+					href='https://www.github.com/anoopraju31'
+					target='_blank'
+					rel='noreferrer'>
+					Anoop Raju
+				</a>
+			</p>
+		</footer>
+	)
 }
 
 export default Footer
