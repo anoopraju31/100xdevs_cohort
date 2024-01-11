4f729a7265ec4eeb51b5857231b0ddcb30d3dbbe
anoopraju31 <anoopraju31@gmail.com>
2024-01-11 14:47:41 +0530

    Week 5 - Assignment: Business Card - Navbar Component - refactor

--- Week 5/assigments/business-card/src/components/Navbar.jsx
+++ Week 5/assigments/business-card/src/components/Navbar.jsx
@@ -12,7 +12,7 @@ const Navbar = () => {
 					Card Generator
 				</Link>
 
-				{pathname !== '/create-card' && (
+				{pathname === '/' && (
 					<Link
 						className='text-white bg-orange-500 hover:bg-orange-600 focus:ring-4 disabled:bg-orange-200 focus:ring-orange-300 font-medium rounded-lg text-sm px-5 py-2.5 focus:outline-none transition-colors duration-300'
 						to='/create-card'>
