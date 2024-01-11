709afc183e9631c2c5ce697964fc0a1d62d182fc
anoopraju31 <anoopraju31@gmail.com>
2024-01-11 14:46:06 +0530

    Week 5 - Assignment: Business Card - Edit Card Page - style change

--- Week 5/assigments/business-card/src/pages/EditCardPage.jsx
+++ Week 5/assigments/business-card/src/pages/EditCardPage.jsx
@@ -2,8 +2,8 @@ import Form from '../components/Form'
 
 const EditCardPage = () => {
 	return (
-		<main className='min-h-screen flex justify-center items-center bg-orange-50'>
-			<div className='flex-shrink-0 w-full max-w-2xl p-10 rounded-xl bg-orange-100'>
+		<main className='min-h-screen flex justify-center items-center bg-orange-50 py-20 px-5'>
+			<div className='flex-shrink-0 w-full max-w-2xl p-5 sm:p-10 rounded-xl bg-orange-100'>
 				<h1 className='text-3xl text-center mb-2 font-bold font-mono text-orange-900'>
 					Edit Card
 				</h1>
