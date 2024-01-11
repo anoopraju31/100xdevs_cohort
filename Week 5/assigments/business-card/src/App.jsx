d93c87d9f2ae93e5d0973736d0af5db968462f5d
anoopraju31 <anoopraju31@gmail.com>
2024-01-11 12:36:02 +0530

    Week 5 - Assignment: Business Card - Sign In page initial

--- Week 5/assigments/business-card/src/App.jsx
+++ Week 5/assigments/business-card/src/App.jsx
@@ -2,13 +2,15 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import CreateCard from './pages/CreateCard'
 import EditCardPage from './pages/EditCardPage'
 import SignUpPage from './pages/SignUpPage'
+import SignInPage from './pages/SignInPage'
 
 const App = () => {
 	return (
 		<BrowserRouter>
 			<Routes>
-				<Route path='/' element={<h1> Home Page </h1>} />
 				<Route path='/sign-up' element={<SignUpPage />} />
+				<Route path='/sign-in' element={<SignInPage />} />
+				<Route path='/' element={<h1> Home Page </h1>} />
 				<Route path='/create-card' element={<CreateCard />} />
 				<Route path='/edit-card' element={<EditCardPage />} />
 			</Routes>
