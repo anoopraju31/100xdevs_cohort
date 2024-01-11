f14a9801aee1f7d18bb5cdc9a3294de2c25e0634
anoopraju31 <anoopraju31@gmail.com>
2024-01-11 15:44:27 +0530

    Week 5 - Assignments - Business Card Backend - index.js refactor

--- Week 5/assigments/business-card-backend/index.js
+++ Week 5/assigments/business-card-backend/index.js
@@ -1,9 +1,9 @@
 const express = require('express')
 const mongoose = require('mongoose')
 const cors = require('cors')
-require('dotenv').config()
 const businessCardRouter = require('./routes/businessCard.routes.js')
 const userRouter = require('./routes/user.routes.js')
+require('dotenv').config()
 
 const app = express()
 const PORT = 5500
@@ -13,13 +13,7 @@ mongoose.connect(MONGODB_CONNECTION_URL)
 
 app.use(cors())
 app.use(express.json())
-app.get('/', (req, res) => {
-	res.json({ message: 'Welcome to Business Card App' })
-})
-
 app.use('/users', userRouter)
 app.use('/business-card', businessCardRouter)
 
-app.listen(PORT, () => {
-	console.log(`Server started at port ${PORT}.`)
-})
+app.listen(PORT, () => console.log(`Server started at port ${PORT}.`))
