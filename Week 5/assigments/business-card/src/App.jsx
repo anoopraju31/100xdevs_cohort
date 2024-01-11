d9d039f859265d9f0f542bb48362d4922f815bdd
anoopraju31 <anoopraju31@gmail.com>
2024-01-11 14:02:23 +0530

    Week 5 - Assignment: Business Card - Navbar Component - initial

--- Week 5/assigments/business-card/src/App.jsx
+++ Week 5/assigments/business-card/src/App.jsx
@@ -1,13 +1,15 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import { Toaster } from 'react-hot-toast'
-import CreateCard from './pages/CreateCard'
-import EditCardPage from './pages/EditCardPage'
+import Navbar from './components/Navbar'
 import SignUpPage from './pages/SignUpPage'
 import SignInPage from './pages/SignInPage'
+import EditCardPage from './pages/EditCardPage'
+import CreateCard from './pages/CreateCard'
 
 const App = () => {
 	return (
 		<BrowserRouter>
+			<Navbar />
 			<Routes>
 				<Route path='/sign-up' element={<SignUpPage />} />
 				<Route path='/sign-in' element={<SignInPage />} />
