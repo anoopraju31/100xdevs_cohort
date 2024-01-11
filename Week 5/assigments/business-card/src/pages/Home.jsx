715465cc5077993549e3086eaeb9ee7b7e4ba278
anoopraju31 <anoopraju31@gmail.com>
2024-01-11 15:06:51 +0530

    Week 5 - Assignment: Business Card - Home Page - styled

--- Week 5/assigments/business-card/src/pages/Home.jsx
+++ Week 5/assigments/business-card/src/pages/Home.jsx
@@ -1,5 +1,20 @@
+import BusinessCard from '../components/BusinessCard'
+import { user } from '../constants'
+
 const Home = () => {
-	return <div>Home</div>
+	return (
+		<main className='min-h-screen flex justify-center  bg-orange-50 py-20 px-5'>
+			<div className='max-w-7xl pt-10 grid gap-5 grid-cols-1 md:grid-cols-2 xl:grid-cols-3'>
+				<BusinessCard {...user} />
+				<BusinessCard {...user} />
+				<BusinessCard {...user} />
+				<BusinessCard {...user} />
+				<BusinessCard {...user} />
+				<BusinessCard {...user} />
+				<BusinessCard {...user} />
+			</div>
+		</main>
+	)
 }
 
 export default Home
