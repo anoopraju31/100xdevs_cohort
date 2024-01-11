b55086fd087f462f5cb9cdc90dd678cee629b5d3
anoopraju31 <anoopraju31@gmail.com>
2024-01-11 12:45:43 +0530

    Week 5 - Assignment: Business Card - Button Component - style change

--- Week 5/assigments/business-card/src/components/Button.jsx
+++ Week 5/assigments/business-card/src/components/Button.jsx
@@ -6,7 +6,7 @@ const Button = (props) => {
 		<button
 			type={type}
 			disabled={isDisabled}
-			className='w-full text-white bg-orange-500 hover:bg-orange-600 focus:ring-4 disabled:bg-orange-200 focus:ring-orange-300 font-medium rounded-lg text-sm px-5 py-2.5 focus:outline-none'>
+			className='w-full text-white bg-orange-500 hover:bg-orange-600 focus:ring-4 disabled:bg-orange-200 focus:ring-orange-300 font-medium rounded-lg text-sm px-5 py-2.5 focus:outline-none transition-colors duration-300'>
 			{title}
 		</button>
 	)
