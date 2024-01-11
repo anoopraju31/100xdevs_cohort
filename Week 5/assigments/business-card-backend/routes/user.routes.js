27ee6c9c10d6c1169c08954c2daf6468b8b60a5c
anoopraju31 <anoopraju31@gmail.com>
2024-01-11 10:39:27 +0530

    Week 5 - Assignment: Business Card Backend - sign in routes added

--- Week 5/assigments/business-card-backend/routes/user.routes.js
+++ Week 5/assigments/business-card-backend/routes/user.routes.js
@@ -1,12 +1,14 @@
 const express = require('express')
-const { signUpSchema } = require('../utills/validations.js')
+const { signUpSchema, signInSchema } = require('../utills/validations.js')
 const User = require('../models/user.models.js')
 const bcrypt = require('bcrypt')
+const jwt = require('jsonwebtoken')
 require('dotenv').config()
 
 const router = express.Router()
 
 const salt = parseInt(process.env.SALT_ROUNDS)
+const secret = process.env.JWT_SECRET
 
 router.get('/', (req, res) => {
 	res.json({ message: 'user route' })
@@ -35,6 +37,41 @@ router.post('/sign-up', async (req, res) => {
 		await newUser.save()
 
 		res.json({ message: 'User created successfully.' })
+	} catch (error) {
+		console.error(error)
+
+		res.status(500).json({ message: 'something went wrong' })
+	}
+})
+
+router.post('/sign-in', async (req, res) => {
+	try {
+		const { email, password } = req.body
+		const validationResponse = signInSchema.safeParse({ email, password })
+
+		if (!validationResponse.success)
+			return res.status(401).json(validationResponse.error.issues[0].message)
+
+		const user = await User.findOne({ email }).select('+password')
+
+		if (!user) return res.status(400).json({ message: 'User not found.' })
+
+		const passwordMatch = await bcrypt.compare(password, user?.password)
+
+		if (!passwordMatch)
+			return res.status(403).json({ message: 'Invalid password' })
+
+		const token = jwt.sign(
+			{
+				name: user.name,
+				email,
+				id: user._id.toString(),
+			},
+			secret,
+			{ expiresIn: '30d' },
+		)
+
+		res.json({ message: 'successfully signed in.', token })
 	} catch (error) {
 		console.error(error)
 
