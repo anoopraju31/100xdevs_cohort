44e4cc638327a30cfa10e2fa64c32b486f7b105d
anoopraju31 <anoopraju31@gmail.com>
2024-01-11 14:37:10 +0530

    Week 5 - Assignment: Business Card - Navbar Component - styled

--- Week 5/assigments/business-card/src/components/Navbar.jsx
+++ Week 5/assigments/business-card/src/components/Navbar.jsx
@@ -1,5 +1,28 @@
+import { Link, useLocation } from 'react-router-dom'
+
 const Navbar = () => {
-	return <div>Navbar</div>
+	const { pathname } = useLocation()
+
+	return (
+		<header className='fixed top-0 left-0 w-full z-50'>
+			<div className='max-w-7xl mx-auto py-5 px-5 xl:px-0 flex justify-between items-center'>
+				<Link
+					to='/'
+					className='text-xl text-center mb-2 font-bold font-mono text-orange-900'>
+					Card Generator
+				</Link>
+
+				{pathname !== '/create-card' && (
+					<Link
+						className='text-white bg-orange-500 hover:bg-orange-600 focus:ring-4 disabled:bg-orange-200 focus:ring-orange-300 font-medium rounded-lg text-sm px-5 py-2.5 focus:outline-none transition-colors duration-300'
+						to='/create-card'>
+						{' '}
+						Create Card{' '}
+					</Link>
+				)}
+			</div>
+		</header>
+	)
 }
 
 export default Navbar
