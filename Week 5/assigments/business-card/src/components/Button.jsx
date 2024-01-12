657d33f628b54ffadc0454aa72d302471923d806
anoopraju31 <anoopraju31@gmail.com>
2024-01-12 09:51:40 +0530

    Week 5 - Assignments - Business Card - Button Component - updated

--- Week 5/assigments/business-card/src/components/Button.jsx
+++ Week 5/assigments/business-card/src/components/Button.jsx
@@ -1,13 +1,13 @@
 import PropTypes from 'prop-types'
 
 const Button = (props) => {
-	const { title, type, isDisabled, handleClick } = props
+	const { title, type, isDisabled, handleClick, style } = props
 	return (
 		<button
 			type={type}
 			disabled={isDisabled}
 			onClick={handleClick}
-			className='w-full text-white bg-orange-500 hover:bg-orange-600 focus:ring-4 disabled:bg-orange-200 focus:ring-orange-300 font-medium rounded-lg text-sm px-5 py-2.5 focus:outline-none transition-colors duration-300'>
+			className={`w-full text-white bg-orange-500 hover:bg-orange-600 focus:ring-4 disabled:bg-orange-200 focus:ring-orange-300 font-medium rounded-lg text-sm px-5 py-2.5 focus:outline-none transition-colors duration-300 ${style}`}>
 			{title}
 		</button>
 	)
@@ -18,6 +18,7 @@ Button.propTypes = {
 	type: PropTypes.string,
 	isDisabled: PropTypes.bool,
 	handleClick: PropTypes.func,
+	style: PropTypes.string,
 }
 
 export default Button
