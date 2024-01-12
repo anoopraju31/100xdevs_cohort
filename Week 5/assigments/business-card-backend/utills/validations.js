a7458c69533e65bfe8c2dde1bec335301499d93c
anoopraju31 <anoopraju31@gmail.com>
2024-01-12 17:33:47 +0530

    Week 5 - Assignments - Business Card Backend - Card Validation refactor

--- Week 5/assigments/business-card-backend/utills/validations.js
+++ Week 5/assigments/business-card-backend/utills/validations.js
@@ -1,28 +1,28 @@
 const zod = require('zod')
 
-const InterestSchema = zod.object({
-	id: zod.string().uuid(),
-	title: zod.string().min(1),
-})
+const InterestSchema = zod.array(zod.object({ title: zod.string().min(1) }))
 
-const SocialSchema = zod.object({
-	id: zod.string().uuid(),
-	title: zod.string().min(1),
-	link: zod.string().url(),
-})
+const SocialSchema = zod.array(
+	zod.object({
+		title: zod.string().min(1),
+		link: zod.string().url(),
+	}),
+)
 
 const cardSchema = zod.object({
 	name: zod.string().min(1),
 	description: zod.string().min(1),
-	interests: zod.array(InterestSchema),
-	socials: zod.array(SocialSchema),
+	interests: InterestSchema,
+	socials: SocialSchema,
 })
 
 const nameSchema = zod.string().min(1, { message: 'Name is required.' })
+
 const emailSchema = zod
 	.string()
 	.min(1, { message: 'Email is required.' })
 	.email('Invalid email.')
+
 const passwordSchema = zod
 	.string()
 	.min(8, { message: 'Password must contain atleast 8 characters.' })
