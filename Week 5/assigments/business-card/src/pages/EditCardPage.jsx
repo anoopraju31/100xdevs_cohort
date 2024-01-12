acb5dcb0a8ef36dea0cb0db30531412001e303b4
anoopraju31 <anoopraju31@gmail.com>
2024-01-12 13:15:21 +0530

    Week 5 - Assignments - Business Card - Edit Card Page - redirected to home

--- Week 5/assigments/business-card/src/pages/EditCardPage.jsx
+++ Week 5/assigments/business-card/src/pages/EditCardPage.jsx
@@ -1,12 +1,23 @@
+import { useContext, useEffect } from 'react'
+import { useNavigate } from 'react-router-dom'
 import Form from '../components/Form'
+import AppContext from '../components/context'
 
 const EditCardPage = () => {
+	const { isEditForm } = useContext(AppContext)
+	const navigate = useNavigate()
+
+	useEffect(() => {
+		if (!isEditForm) return navigate('/')
+	}, [isEditForm, navigate])
+
 	return (
 		<main className='min-h-screen flex justify-center items-center bg-orange-50 py-20 px-5'>
 			<div className='flex-shrink-0 w-full max-w-2xl p-5 sm:p-10 rounded-xl bg-orange-100'>
 				<h1 className='text-3xl text-center mb-2 font-bold font-mono text-orange-900'>
 					Edit Card
 				</h1>
+
 				<Form />
 			</div>
 		</main>
