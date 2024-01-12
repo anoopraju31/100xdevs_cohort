5e41872002907b4d7391522094c7c0743a733892
anoopraju31 <anoopraju31@gmail.com>
2024-01-12 13:10:15 +0530

    Week 5 - Assignments - Business Card - Not Page - styled

--- Week 5/assigments/business-card/src/pages/NotFoundPage.jsx
+++ Week 5/assigments/business-card/src/pages/NotFoundPage.jsx
@@ -1,5 +1,21 @@
+import { Link } from 'react-router-dom'
+
 const NotFoundPage = () => {
-	return <div>NotFoundPage</div>
+	return (
+		<main className='h-screen flex justify-center items-center flex-col bg-orange-50'>
+			<h1 className='text-9xl font-extrabold font-mono tracking-tight text-center text-orange-900'>
+				404 <br className='md:hidden' /> Page Not Found
+			</h1>
+			<p className='text-xl text-orange-900 font-mono mt-10 font-bold'>
+				{' '}
+				Return to{' '}
+				<Link className='underline' to='/'>
+					{' '}
+					Home{' '}
+				</Link>{' '}
+			</p>
+		</main>
+	)
 }
 
 export default NotFoundPage
