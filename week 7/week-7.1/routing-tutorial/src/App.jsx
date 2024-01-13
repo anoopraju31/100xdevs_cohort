6391e36c0b1a8accc34aa63e9d56c89b9f16d9f8
anoopraju31 <anoopraju31@gmail.com>
2024-01-13 19:37:29 +0530

    Week 7.1 Routing Tutorial - Dashboard Page added

--- week 7/week-7.1/routing-tutorial/src/App.jsx
+++ week 7/week-7.1/routing-tutorial/src/App.jsx
@@ -1,6 +1,7 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Home from './pages/Home'
 import Contact from './pages/Contact'
+import Dashboard from './pages/Dashboard'
 
 const App = () => {
 	return (
@@ -8,6 +9,7 @@ const App = () => {
 			<Routes>
 				<Route path='/' element={<Home />} />
 				<Route path='/contact' element={<Contact />} />
+				<Route path='/dashboard' element={<Dashboard />} />
 			</Routes>
 		</BrowserRouter>
 	)
