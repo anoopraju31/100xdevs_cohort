0cbb457e78c086d816498dcca7fa6b15d2629e1b
anoopraju31 <anoopraju31@gmail.com>
2024-01-13 19:43:07 +0530

    Week 7.1 Routing Tutorial - Dashboard Page updated

--- week 7/week-7.1/routing-tutorial/src/pages/Dashboard.jsx
+++ week 7/week-7.1/routing-tutorial/src/pages/Dashboard.jsx
@@ -1,5 +1,19 @@
+import { useNavigate } from 'react-router-dom'
+
 const Dashboard = () => {
-	return <div>Dashboard</div>
+	const navigate = useNavigate()
+	const goToHome = () => navigate('/')
+
+	return (
+		<div className='h-screen flex justify-center items-center bg-orange-100'>
+			<button
+				className='text-white bg-orange-500 hover:bg-orange-600 focus:ring-4 disabled:bg-orange-200 focus:ring-orange-300 font-medium rounded-lg text-sm px-5 py-2.5 focus:outline-none transition-colors duration-300'
+				onClick={goToHome}>
+				{' '}
+				Go To Home{' '}
+			</button>
+		</div>
+	)
 }
 
 export default Dashboard
