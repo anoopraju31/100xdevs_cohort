3216b5dbc637d8b26ee369a20b95b5c3ba6cb4bb
anoopraju31 <anoopraju31@gmail.com>
2024-01-14 00:51:27 +0530

    Week 6 - Assignments - 2. useRef - Assignment 1 added

--- week 6/assignments/3-use-ref/src/App.jsx
+++ week 6/assignments/3-use-ref/src/App.jsx
@@ -3,12 +3,12 @@ import { Assignment1 } from './components/Assignment1'
 import { Assignment2 } from './components/Assignment2'
 
 function App() {
-  return (
-    <>
-      <Assignment1 />
-      {/* <Assignment2 /> */}
-    </>
-  )
+	return (
+		<>
+			<Assignment1 />
+			<Assignment2 />
+		</>
+	)
 }
 
 export default App
--- week 6/assignments/3-use-ref/src/components/Assignment1.jsx
+++ week 6/assignments/3-use-ref/src/components/Assignment1.jsx
@@ -1,21 +1,24 @@
-import { useEffect } from "react";
+import { useCallback, useEffect, useRef } from 'react'
 
 // Create a component with a text input field and a button. When the component mounts or the button is clicked, automatically focus the text input field using useRef.
 
 export function Assignment1() {
+	const inputRef = useRef(null)
 
-    useEffect(() => {
+	useEffect(() => {
+		if (!inputRef.current) return
 
-    }, []);
+		inputRef.current.focus()
+	}, [])
 
-    const handleButtonClick = () => {
+	const handleButtonClick = useCallback(() => {
+		inputRef.current.focus()
+	}, [])
 
-    };
-
-    return (
-        <div>
-            <input type="text" placeholder="Enter text here" />
-            <button onClick={handleButtonClick}>Focus Input</button>
-        </div>
-    );
-};
+	return (
+		<div>
+			<input ref={inputRef} type='text' placeholder='Enter text here' />
+			<button onClick={handleButtonClick}>Focus Input</button>
+		</div>
+	)
+}
