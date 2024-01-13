e8bddbc80fd64693e26f13752705649adc767c0a
anoopraju31 <anoopraju31@gmail.com>
2024-01-13 13:20:04 +0530

    Week 6.2 useEffect hook tutorial - todo added

--- week 6/week-6.2/useEffect-hook-tutorial/src/App.jsx
+++ week 6/week-6.2/useEffect-hook-tutorial/src/App.jsx
@@ -1,5 +1,37 @@
+import { useEffect, useState } from 'react'
+import PropTypes from 'prop-types'
+import axios from 'axios'
+
 const App = () => {
-	return <div>App</div>
+	const [todos, setTodos] = useState([])
+
+	useEffect(() => {
+		axios
+			.get('https://sum-server.100xdevs.com/todos')
+			.then((res) => setTodos(res.data.todos))
+	}, [])
+
+	return (
+		<>
+			{todos.map(({ id, title, description }) => (
+				<Todo key={id} title={title} description={description} />
+			))}
+		</>
+	)
+}
+
+const Todo = ({ title, description }) => {
+	return (
+		<div>
+			<h3> {title} </h3>
+			<p> {description} </p>
+		</div>
+	)
+}
+
+Todo.propTypes = {
+	title: PropTypes.string.isRequired,
+	description: PropTypes.string.isRequired,
 }
 
 export default App
