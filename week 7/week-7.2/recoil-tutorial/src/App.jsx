cefe8f366c933a98f9316fdd68349db420080690
anoopraju31 <anoopraju31@gmail.com>
2024-01-17 21:29:46 +0530

    Week 7.2 Recoil tutorial - tailwind css added

--- week 7/week-7.2/recoil-tutorial/src/App.jsx
+++ week 7/week-7.2/recoil-tutorial/src/App.jsx
@@ -1,7 +1,9 @@
-import React from 'react'
-
 const App = () => {
-	return <div>App</div>
+	return (
+		<div>
+			<h1 className='text-3xl font-bold underline'>Hello world!</h1>
+		</div>
+	)
 }
 
 export default App
