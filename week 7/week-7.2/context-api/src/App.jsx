860e09151a0ad3dd57734cd64b7f9738c082f010
anoopraju31 <anoopraju31@gmail.com>
2024-01-17 21:05:57 +0530

    Week 7.2 - Context API continue: Counter added

--- week 7/week-7.2/context-api/src/App.jsx
+++ week 7/week-7.2/context-api/src/App.jsx
@@ -1,7 +1,46 @@
-import React from 'react'
+import { useContext, useState } from 'react'
+import { CountContext } from './context'
 
 const App = () => {
-	return <div>App</div>
+	const [count, setCount] = useState(0)
+	return (
+		<CountContext.Provider value={count}>
+			<Count setCount={setCount} />
+		</CountContext.Provider>
+	)
+}
+
+const Count = ({ setCount }) => {
+	return (
+		<div
+			style={{
+				background: '#fc3',
+				height: '50vh',
+				width: '50vh',
+				display: 'flex',
+				flexDirection: 'column',
+				gap: '50px',
+				alignItems: 'center',
+				justifyContent: 'center',
+			}}>
+			<CounterRenderer />
+			<Button setCount={setCount} />
+		</div>
+	)
+}
+
+const CounterRenderer = () => {
+	const count = useContext(CountContext)
+	return <div> {count} </div>
+}
+
+const Button = ({ setCount }) => {
+	return (
+		<button type='button' onClick={() => setCount((prev) => prev + 1)}>
+			{' '}
+			Increment{' '}
+		</button>
+	)
 }
 
 export default App
