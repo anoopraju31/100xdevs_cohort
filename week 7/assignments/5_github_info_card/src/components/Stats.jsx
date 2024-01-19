40824b7466d31d8c1fa5f7df18d4424b233cfe75
anoopraju31 <anoopraju31@gmail.com>
2024-01-19 22:17:08 +0530

    Week 7 - Assignments - 5. Github Info Card - Stats Component - style refactor

--- week 7/assignments/5_github_info_card/src/components/Stats.jsx
+++ week 7/assignments/5_github_info_card/src/components/Stats.jsx
@@ -2,10 +2,15 @@ import PropTypes from 'prop-types'
 
 const Stats = (props) => {
 	const { stat, title } = props
+
 	return (
-		<div className='flex flex-col items-center gap-2 '>
-			<p className='text-2xl font-bold text-gray-800'> {stat} </p>
-			<p className='text-gray-600 capitalize'> {title} </p>
+		<div className='w-full p-5 rounded-xl bg-orange-100 flex flex-col justify-center items-center gap-2 '>
+			<p className='text-2xl font-extrabold font-sans text-orange-900'>
+				{stat}
+			</p>
+			<p className='text-orange-700 font-medium capitalize whitespace-nowrap'>
+				{title}
+			</p>
 		</div>
 	)
 }
