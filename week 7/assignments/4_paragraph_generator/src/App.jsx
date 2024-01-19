183ec45dbbd1c1f4743c2cf36091cc807c4e5d5a
anoopraju31 <anoopraju31@gmail.com>
2024-01-19 18:41:29 +0530

    Week 7 Assignments - 4. paragraph generator - Recoil added

--- week 7/assignments/4_paragraph_generator/src/App.jsx
+++ week 7/assignments/4_paragraph_generator/src/App.jsx
@@ -1,7 +1,12 @@
+import { RecoilRoot } from 'recoil'
 import Home from './pages/Home'
 
 const App = () => {
-	return <Home />
+	return (
+		<RecoilRoot>
+			<Home />
+		</RecoilRoot>
+	)
 }
 
 export default App
