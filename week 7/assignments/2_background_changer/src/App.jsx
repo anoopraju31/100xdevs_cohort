3d9741373618af091892fec13c06c582bcd672fb
anoopraju31 <anoopraju31@gmail.com>
2024-01-19 15:00:47 +0530

    Week 7 Assignments - 2. Background Changer - Button component - initial

--- week 7/assignments/2_background_changer/src/App.jsx
+++ week 7/assignments/2_background_changer/src/App.jsx
@@ -1,8 +1,16 @@
+import Button from './components/Button'
+
 const App = () => {
 	return (
 		<main className='h-screen flex justify-center items-end'>
 			<div className='p-5 flex items-center gap-4'>
-				<h1 className='text-3xl font-bold underline'>Hello world!</h1>
+				<Button />
+				<Button />
+				<Button />
+				<Button />
+				<Button />
+				<Button />
+				<Button />
 			</div>
 		</main>
 	)
