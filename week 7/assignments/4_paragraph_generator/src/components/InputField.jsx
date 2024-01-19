5c07020c479c0e243628ba286319b659bd8275ac
anoopraju31 <anoopraju31@gmail.com>
2024-01-19 18:30:21 +0530

    Week 7 Assignments - 4. paragraph generator - InputField component - styled

--- week 7/assignments/4_paragraph_generator/src/components/InputField.jsx
+++ week 7/assignments/4_paragraph_generator/src/components/InputField.jsx
@@ -1,5 +1,44 @@
-const InputField = () => {
-	return <div>InputField</div>
+import PropTypes from 'prop-types'
+
+const InputField = (props) => {
+	const {
+		id,
+		name,
+		label,
+		placeholder,
+		type,
+		value,
+		handleChange,
+		isDisabled,
+	} = props
+	return (
+		<div>
+			<label htmlFor={id} className='sr-only'>
+				{label}
+			</label>
+			<input
+				id={id}
+				name={name}
+				type={type}
+				disabled={isDisabled}
+				placeholder={placeholder}
+				value={value}
+				onChange={handleChange}
+				className='bg-transparent border outline-none border-orange-300 text-orange-900 text-sm rounded-lg focus:ring-orange-500 focus:border-orange-500 block w-full p-2.5 placeholder:text-orange-800/80 transition-colors duration-300'
+			/>
+		</div>
+	)
+}
+
+InputField.propTypes = {
+	name: PropTypes.string,
+	label: PropTypes.string,
+	id: PropTypes.string,
+	placeholder: PropTypes.string,
+	type: PropTypes.string,
+	handleChange: PropTypes.func,
+	value: PropTypes.string,
+	isDisabled: PropTypes.bool,
 }
 
 export default InputField
