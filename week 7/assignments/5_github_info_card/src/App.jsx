ae712f724776c1664b161d60f1dd0ad080511df7
anoopraju31 <anoopraju31@gmail.com>
2024-01-19 22:16:29 +0530

    Week 7 - Assignments - 5. Github Info Card - App Component - style refactor

--- week 7/assignments/5_github_info_card/src/App.jsx
+++ week 7/assignments/5_github_info_card/src/App.jsx
@@ -4,7 +4,7 @@ import Form from './components/Form'
 const App = () => {
 	return (
 		<main className='min-h-screen bg-orange-100 py-10 px-4 flex flex-col lg:flex-row lg:justify-center item-center lg:items-start gap-10'>
-			<div className='max-w-2xl mx-auto lg:mx-0 border p-10 h-fit border-orange-500 rounded-xl flex flex-col gap-10'>
+			<div className='max-w-2xl mx-auto lg:mx-0 p-10 h-fit rounded-xl flex flex-col gap-10'>
 				<h1 className='text-center text-4xl font-bold font-mono capitalize text-orange-800'>
 					{' '}
 					Github Profile Viewer{' '}
