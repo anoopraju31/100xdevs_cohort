8954500d74aabfb7a895ee0a98a67706e74be17a
anoopraju31 <anoopraju31@gmail.com>
2024-01-19 15:34:54 +0530

    Week 7 Assignments - 2. Background Changer - backgrounds constants added

--- week 7/assignments/2_background_changer/src/pages/Home.jsx
+++ week 7/assignments/2_background_changer/src/pages/Home.jsx
@@ -1,6 +1,7 @@
 import { useRecoilValue } from 'recoil'
 import Button from '../components/Button'
 import { backgroundAtom } from '../store/atoms/background'
+import { backgrounds } from '../utils/constants'
 
 const Home = () => {
 	const background = useRecoilValue(backgroundAtom)
@@ -9,37 +10,9 @@ const Home = () => {
 		<main
 			className={`h-screen p-5 flex justify-center items-end ${background}`}>
 			<div className='p-5 bg-white/40 backdrop-blur-lg rounded-lg shadow-lg flex items-center gap-4'>
-				<Button
-					title='red'
-					color='bg-red-500'
-					styles='!bg-red-500 ! focus:ring-red-300'
-				/>
-				<Button
-					title='yellow'
-					color='bg-yellow-500'
-					styles='!bg-yellow-500 ! focus:ring-yellow-300'
-				/>
-				<Button
-					title='black'
-					color='bg-black'
-					styles='!bg-black ! focus:ring-black/30'
-				/>
-				<Button
-					title='purple'
-					color='bg-purple-500'
-					styles='!bg-purple-500 ! focus:ring-purple-300'
-				/>
-				<Button
-					title='green'
-					color='bg-green-500'
-					styles='!bg-green-500 ! focus:ring-green-300'
-				/>
-				<Button
-					title='blue'
-					color='bg-blue-500'
-					styles='!bg-blue-500 ! focus:ring-blue-300'
-				/>
-				<Button title='default' color='bg-orange-500' />
+				{backgrounds.map(({ id, title, color, styles }) => (
+					<Button key={id} title={title} color={color} styles={styles} />
+				))}
 			</div>
 		</main>
 	)
---
+++ week 7/assignments/2_background_changer/src/utils/constants.js
@@ -0,0 +1,44 @@
+export const backgrounds = [
+	{
+		id: '1',
+		title: 'Red',
+		color: 'bg-red-500',
+		styles: '!bg-red-500 ! focus:ring-red-300',
+	},
+	{
+		id: '2',
+		title: 'Yellow',
+		color: 'bg-yellow-500',
+		styles: '!bg-yellow-500 ! focus:ring-yellow-300',
+	},
+	{
+		id: '3',
+		title: 'Black',
+		color: 'bg-black',
+		styles: '!bg-black ! focus:ring-black/30',
+	},
+	{
+		id: '4',
+		title: 'Purple',
+		color: 'bg-purple-500',
+		styles: '!bg-purple-500 ! focus:ring-purple-300',
+	},
+	{
+		id: '5',
+		title: 'Green',
+		color: 'bg-green-500',
+		styles: '!bg-green-500 ! focus:ring-green-300',
+	},
+	{
+		id: '6',
+		title: 'Blue',
+		color: 'bg-blue-500',
+		styles: '!bg-blue-500 ! focus:ring-blue-300',
+	},
+	{
+		id: '7',
+		title: 'Default',
+		color: 'bg-orange-500',
+		styles: 'bg-orange-500 ! focus:ring-orange-300',
+	},
+]
