097e4faf8ddab7228f28521c74336c62e71289c8
anoopraju31 <anoopraju31@gmail.com>
2024-01-19 15:21:07 +0530

    Week 7 Assignments - 2. Background Changer - Home Page added

--- week 7/assignments/2_background_changer/src/App.jsx
+++ week 7/assignments/2_background_changer/src/App.jsx
@@ -1,18 +1,11 @@
-import Button from './components/Button'
+import { RecoilRoot } from 'recoil'
+import Home from './pages/Home'
 
 const App = () => {
 	return (
-		<main className='h-screen flex justify-center items-end'>
-			<div className='p-5 flex items-center gap-4'>
-				<Button title='red' color='bg-red-500' styles='bg-red-500' />
-				<Button title='yellow' color='bg-yellow-500' styles='bg-yellow-500' />
-				<Button title='black' color='bg-black' styles='bg-black' />
-				<Button title='purple' color='bg-purple-500' styles='bg-purple-500' />
-				<Button title='green' color='bg-green-500' styles='bg-green-500' />
-				<Button title='blue' color='bg-blue-500' styles='bg-blue-500' />
-				<Button title='default' color='bg-orange-500' styles='bg-orange-500' />
-			</div>
-		</main>
+		<RecoilRoot>
+			<Home />
+		</RecoilRoot>
 	)
 }
 
