ad28c2a9b75cfcb30d4248953a99c928264bc50a
anoopraju31 <anoopraju31@gmail.com>
2024-01-19 14:59:36 +0530

    Week 7 Assignments - 2. Background Changer - App component style change

--- week 7/assignments/2_background_changer/src/App.jsx
+++ week 7/assignments/2_background_changer/src/App.jsx
@@ -1,8 +1,10 @@
 const App = () => {
 	return (
-		<div>
-			<h1 className='text-3xl font-bold underline'>Hello world!</h1>
-		</div>
+		<main className='h-screen flex justify-center items-end'>
+			<div className='p-5 flex items-center gap-4'>
+				<h1 className='text-3xl font-bold underline'>Hello world!</h1>
+			</div>
+		</main>
 	)
 }
 
