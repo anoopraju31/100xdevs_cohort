9284f52b0c9dfbb321c863463cf1b313a1fb5866
anoopraju31 <anoopraju31@gmail.com>
2024-01-19 19:11:40 +0530

    Week 7 Assignments - 4. paragraph generator - Paragraph container component - styled

--- week 7/assignments/4_paragraph_generator/src/components/ParagraphContainer.jsx
+++ week 7/assignments/4_paragraph_generator/src/components/ParagraphContainer.jsx
@@ -1,5 +1,14 @@
 const ParagraphContainer = () => {
-	return <div>ParagraphContainer</div>
+	return (
+		<section className='w-full max-w-3xl mx-auto p-10 text-lg font-mono text-orange-800'>
+			<p>
+				Lorem ipsum dolor, sit amet consectetur adipisicing elit. Esse
+				dignissimos a dicta obcaecati tempore eligendi, nesciunt adipisci
+				pariatur sunt omnis, eveniet sequi veniam tenetur? Excepturi at incidunt
+				velit iure eveniet!
+			</p>
+		</section>
+	)
 }
 
 export default ParagraphContainer
