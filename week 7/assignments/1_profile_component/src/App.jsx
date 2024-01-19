628e757583b8be131f63f49d58c8ccda5c5cd246
anoopraju31 <anoopraju31@gmail.com>
2024-01-19 13:36:34 +0530

    Week 7 Assignments - 1. Profile Component: Profile Component - initial

--- week 7/assignments/1_profile_component/src/App.jsx
+++ week 7/assignments/1_profile_component/src/App.jsx
@@ -1,7 +1,9 @@
+import Profile from './components/Profile'
+
 const App = () => {
 	return (
 		<div>
-			<h1 className='text-3xl font-bold underline'>Hello world!</h1>
+			<Profile />
 		</div>
 	)
 }
