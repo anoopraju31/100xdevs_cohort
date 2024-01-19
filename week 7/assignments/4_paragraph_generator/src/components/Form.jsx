80b6b73605a798d047ebd31943643a8f5e951f68
anoopraju31 <anoopraju31@gmail.com>
2024-01-19 18:36:06 +0530

    Week 7 Assignments - 4. paragraph generator - Form component - styled

--- week 7/assignments/4_paragraph_generator/src/components/Form.jsx
+++ week 7/assignments/4_paragraph_generator/src/components/Form.jsx
@@ -3,7 +3,7 @@ import InputField from './InputField'
 
 const Form = () => {
 	return (
-		<form>
+		<form className='w-full max-w-3xl mx-auto p-10 flex flex-col gap-4'>
 			<InputField />
 			<Button title='Generate' type='button' />
 		</form>
