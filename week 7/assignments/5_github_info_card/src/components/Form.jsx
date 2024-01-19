dfb1c1d414d4978dc30a83bff1678a691e43ef88
anoopraju31 <anoopraju31@gmail.com>
2024-01-19 22:09:09 +0530

    Week 7 - Assignments - 5. Github Info Card - Form Component - style refactor

--- week 7/assignments/5_github_info_card/src/components/Form.jsx
+++ week 7/assignments/5_github_info_card/src/components/Form.jsx
@@ -3,7 +3,7 @@ import InputField from './InputField'
 
 const Form = () => {
 	return (
-		<form className='max-w-2xl mx-auto p-10 flex flex-col gap-4'>
+		<form className='flex flex-col gap-4'>
 			<InputField />
 			<Button title='Search' />
 		</form>
