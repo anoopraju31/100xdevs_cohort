976db68e4ae68b31012ff32676085cbe95e6f1a8
anoopraju31 <anoopraju31@gmail.com>
2024-01-19 18:50:06 +0530

    Week 7 Assignments - 4. paragraph generator - Form component - interaction added

--- week 7/assignments/4_paragraph_generator/src/components/Form.jsx
+++ week 7/assignments/4_paragraph_generator/src/components/Form.jsx
@@ -1,11 +1,32 @@
+import { useRecoilState } from 'recoil'
 import Button from './Button'
 import InputField from './InputField'
+import { paragraphLengthAtom } from '../store/atom/paragraphLengthAtom'
 
 const Form = () => {
+	const [paragraphLength, setParagraphLength] =
+		useRecoilState(paragraphLengthAtom)
+	const handleChange = (e) => setParagraphLength(e.target.value)
+	const handleSubmit = (e) => {
+		e.preventDefault()
+
+		console.log(paragraphLength)
+	}
+
 	return (
-		<form className='w-full max-w-3xl mx-auto p-10 flex flex-col gap-4'>
-			<InputField />
-			<Button title='Generate' type='button' />
+		<form
+			onSubmit={handleSubmit}
+			className='w-full max-w-3xl mx-auto p-10 flex flex-col gap-4'>
+			<InputField
+				id='paragraphLength'
+				label='Paragraph Length'
+				name='paragraphLength'
+				placeholder='Enter number of words'
+				value={paragraphLength}
+				handleChange={handleChange}
+				type='number'
+			/>
+			<Button title='Generate' type='submit' />
 		</form>
 	)
 }
