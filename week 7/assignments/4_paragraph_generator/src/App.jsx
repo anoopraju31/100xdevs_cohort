dd27ea6950458ca4c53e35ee407076e34cfeec54
anoopraju31 <anoopraju31@gmail.com>
2024-01-19 18:23:14 +0530

    Week 7 Assignments - 4. paragraph generator - Form component - initial

--- week 7/assignments/4_paragraph_generator/src/App.jsx
+++ week 7/assignments/4_paragraph_generator/src/App.jsx
@@ -1,8 +1,10 @@
+import Form from './components/Form'
+
 const App = () => {
 	return (
-		<div>
-			<h1 className='text-3xl font-bold underline'>Hello world!</h1>
-		</div>
+		<main>
+			<Form />
+		</main>
 	)
 }
 
