12f66c85d73db38e1f9508f4f997ced855039b1f
anoopraju31 <anoopraju31@gmail.com>
2024-01-19 15:09:35 +0530

    Week 7 Assignments - 2. Background Changer - Button component - styled

--- week 7/assignments/2_background_changer/src/App.jsx
+++ week 7/assignments/2_background_changer/src/App.jsx
@@ -4,13 +4,13 @@ const App = () => {
 	return (
 		<main className='h-screen flex justify-center items-end'>
 			<div className='p-5 flex items-center gap-4'>
-				<Button />
-				<Button />
-				<Button />
-				<Button />
-				<Button />
-				<Button />
-				<Button />
+				<Button title='red' color='bg-red-500' styles='bg-red-500' />
+				<Button title='yellow' color='bg-yellow-500' styles='bg-yellow-500' />
+				<Button title='black' color='bg-black' styles='bg-black' />
+				<Button title='purple' color='bg-purple-500' styles='bg-purple-500' />
+				<Button title='green' color='bg-green-500' styles='bg-green-500' />
+				<Button title='blue' color='bg-blue-500' styles='bg-blue-500' />
+				<Button title='default' color='bg-orange-500' styles='bg-orange-500' />
 			</div>
 		</main>
 	)
--- week 7/assignments/2_background_changer/src/components/Button.jsx
+++ week 7/assignments/2_background_changer/src/components/Button.jsx
@@ -1,5 +1,25 @@
-const Button = () => {
-	return <div>Button</div>
+import PropTypes from 'prop-types'
+
+const Button = (props) => {
+	const { title, color, styles } = props
+	const handleClick = () => {
+		console.log(color)
+	}
+
+	return (
+		<button
+			type='button'
+			onClick={handleClick}
+			className={`text-white bg-orange-500 hover:bg-orange-600 focus:ring-4 disabled:bg-orange-200 focus:ring-orange-300 font-medium rounded-lg text-sm px-5 py-2.5 focus:outline-none transition-colors duration-300 ${styles}`}>
+			{title}
+		</button>
+	)
+}
+
+Button.propTypes = {
+	title: PropTypes.string.isRequired,
+	color: PropTypes.string.isRequired,
+	styles: PropTypes.string.isRequired,
 }
 
 export default Button
