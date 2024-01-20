acf1bcb9b038d9de0fc56ce22483c051cf52673f
anoopraju31 <anoopraju31@gmail.com>
2024-01-20 21:08:51 +0530

    Week 7 - Assignment - 4. Paragraph generator - Recoil atoms - refactor

--- week 7/assignments/4_paragraph_generator/src/store/atom/paragraphLengthAtom.js
+++ week 7/assignments/4_paragraph_generator/src/store/atom/paragraphLengthAtom.js
@@ -1,6 +1,18 @@
-import { atom } from 'recoil'
+import { atom, selector } from 'recoil'
+import { generateRandomParagraph } from '../../helpers'
 
 export const paragraphLengthAtom = atom({
 	key: 'paragraphLengthAtom',
 	default: '',
+})
+
+export const generateParagraphSelector = selector({
+	key: 'generateParagraphSelector',
+	get: ({ get }) => {
+		const paragraphLength = get(paragraphLengthAtom)
+
+		if (!paragraphLength) return null
+
+		return generateRandomParagraph(parseInt(paragraphLength))
+	},
 })
