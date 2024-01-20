b4b613c611f03a36ad02819d04b997874b6b7abe
anoopraju31 <anoopraju31@gmail.com>
2024-01-20 20:55:44 +0530

    Week 7 Assignment - 5. Github info card - Home Page Refactor

--- week 7/assignments/5_github_info_card/src/pages/Home.jsx
+++ week 7/assignments/5_github_info_card/src/pages/Home.jsx
@@ -1,10 +1,11 @@
 import Form from '../components/Form'
 import Card from '../components/Card'
+import { Suspense } from 'react'
 
 const Home = () => {
 	return (
 		<main className='min-h-screen bg-orange-100 py-10 px-4 flex flex-col lg:flex-row lg:justify-center item-center lg:items-start gap-10'>
-			<div className='max-w-2xl mx-auto lg:mx-0 p-10 h-fit rounded-xl flex flex-col gap-10'>
+			<div className='max-w-2xl mx-auto lg:mx-0 py-10 md:px-10 h-fit rounded-xl flex flex-col gap-10'>
 				<h1 className='text-center text-4xl font-bold font-mono capitalize text-orange-800'>
 					{' '}
 					Github Profile Viewer{' '}
@@ -13,7 +14,15 @@ const Home = () => {
 			</div>
 
 			<section>
-				<Card />
+				<Suspense
+					fallback={
+						<h1 className='lg:w-[512px] mx-auto p-10 text-center text-lg font-mono font-medium text-orange-800'>
+							{' '}
+							Loading...{' '}
+						</h1>
+					}>
+					<Card />
+				</Suspense>
 			</section>
 		</main>
 	)
