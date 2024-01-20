9b1866837a3b48aadc8d98c7fe753d4f5d1b2b1c
anoopraju31 <anoopraju31@gmail.com>
2024-01-20 22:59:59 +0530

    Week 7 Assignment - 6. OTP Login - OTP Form Component - styled

--- week 7/assignments/6_otp_login/src/components/OtpForm.jsx
+++ week 7/assignments/6_otp_login/src/components/OtpForm.jsx
@@ -1,5 +1,43 @@
+import Button from './Button'
+import InputField from './InputField'
+
 const OtpForm = () => {
-	return <div>OtpForm</div>
+	return (
+		<section className='w-full max-w-sm p-10 rounded-xl border border-orange-300'>
+			<h1 className='text-3xl mb-8 font-bold font-mono text-center text-orange-600'>
+				Login via OTP
+			</h1>
+			<form className='flex flex-col gap-6 flex-1'>
+				<div className='px-6 flex gap-4 items-center '>
+					<InputField
+						id='otp-one'
+						label='First OTP Number'
+						name='first'
+						type='text'
+					/>
+					<InputField
+						id='otp-second'
+						label='Second OTP Number'
+						name='second'
+						type='text'
+					/>
+					<InputField
+						id='otp-third'
+						label='Third OTP Number'
+						name='third'
+						type='text'
+					/>
+					<InputField
+						id='otp-forth'
+						label='Forth OTP Number'
+						name='forth'
+						type='text'
+					/>
+				</div>
+				<Button title='Login' />
+			</form>
+		</section>
+	)
 }
 
 export default OtpForm
