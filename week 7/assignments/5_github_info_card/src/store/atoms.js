235840fa21526764721e4ae52eb277f91214d0d8
anoopraju31 <anoopraju31@gmail.com>
2024-01-20 20:49:32 +0530

    Week 7 - Assignment - 5. Github Info Card - Form Component - github profile selector added

--- week 7/assignments/5_github_info_card/src/store/atoms.js
+++ week 7/assignments/5_github_info_card/src/store/atoms.js
@@ -1,6 +1,20 @@
-import { atom } from 'recoil'
+import { atom, selector } from 'recoil'
 
 export const usernameAtom = atom({
 	key: 'usernameAtom',
 	default: '',
+})
+
+export const githubProfileSelector = selector({
+	key: 'githubProfileSelector',
+	get: async ({ get }) => {
+		const username = get(usernameAtom)
+
+		if (!username) return null
+
+		const res = await fetch(`https://api.github.com/users/${username}`)
+		const data = await res.json()
+
+		return data
+	},
 })
