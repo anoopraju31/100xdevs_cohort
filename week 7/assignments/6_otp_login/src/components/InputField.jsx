0a60a9f58b1f56f1ba76fb8a1a292984a83e87c1
anoopraju31 <anoopraju31@gmail.com>
2024-01-20 22:39:19 +0530

    Week 7 Assignment - 6. OTP Login - Input Field Component - styled

--- week 7/assignments/6_otp_login/src/components/InputField.jsx
+++ week 7/assignments/6_otp_login/src/components/InputField.jsx
@@ -1,5 +1,38 @@
-const InputField = () => {
-	return <div>InputField</div>
+import PropTypes from 'prop-types'
+
+const InputField = (props) => {
+	const { label, id, name, type, placeholder, value, handleChange } = props
+
+	return (
+		<>
+			<label htmlFor={id} className='sr-only'>
+				{label}
+			</label>
+			<input
+				id={id}
+				name={name}
+				type={type}
+				placeholder={placeholder}
+				value={value}
+				onChange={handleChange}
+				className='bg-transparent border outline-none border-orange-300 text-orange-900 text-sm rounded-lg focus:ring-orange-500 focus:border-orange-500 block w-full p-2.5 placeholder:text-orange-800/80 transition-colors duration-300'
+			/>
+		</>
+	)
+}
+
+InputField.propTypes = {
+	label: PropTypes.string.isRequired,
+	id: PropTypes.string.isRequired,
+	name: PropTypes.string.isRequired,
+	type: PropTypes.oneOf(['text', 'email', 'number', 'url']).isRequired,
+	placeholder: PropTypes.string.isRequired,
+	value: PropTypes.oneOfType([PropTypes.string, PropTypes.number]),
+	handleChange: PropTypes.func.isRequired,
+}
+
+InputField.defaultProp = {
+	type: 'text',
 }
 
 export default InputField
