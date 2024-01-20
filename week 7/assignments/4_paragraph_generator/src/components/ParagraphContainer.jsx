58957689d73a9f00dcb1a56e7e23c976ce738f0e
anoopraju31 <anoopraju31@gmail.com>
2024-01-20 21:10:16 +0530

    Week 7 - Assignment - 4. Paragraph generator - Paragraph Container Component - refactor

--- week 7/assignments/4_paragraph_generator/src/components/ParagraphContainer.jsx
+++ week 7/assignments/4_paragraph_generator/src/components/ParagraphContainer.jsx
@@ -1,15 +1,10 @@
-import { useRecoilValue, useSetRecoilState } from 'recoil'
-import {
-	generateParagraphSelector,
-	isFormSubmitAtom,
-} from '../store/atom/isFormSubmitAtom'
+import { useRecoilValue } from 'recoil'
+import { generateParagraphSelector } from '../store/atom/paragraphLengthAtom'
 
 const ParagraphContainer = () => {
 	const generatedParagraph = useRecoilValue(generateParagraphSelector)
-	const setIsSubmitting = useSetRecoilState(isFormSubmitAtom)
 
 	if (generatedParagraph == null) return null
-	else setIsSubmitting(false)
 
 	return (
 		<section className='w-full max-w-3xl mx-auto p-10 text-lg font-mono text-orange-800'>
