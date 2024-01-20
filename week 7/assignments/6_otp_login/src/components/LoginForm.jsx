372aecd0d25aa444a5cf7f606df20d2da6822582
anoopraju31 <anoopraju31@gmail.com>
2024-01-20 22:49:16 +0530

    Week 7 Assignment - 6. OTP Login - Login Form Component - styled

--- week 7/assignments/6_otp_login/src/components/LoginForm.jsx
+++ week 7/assignments/6_otp_login/src/components/LoginForm.jsx
@@ -3,10 +3,21 @@ import InputField from './InputField'
 
 const LoginForm = () => {
 	return (
-		<div>
-			<InputField />
-			<Button title='Send OTP' />
-		</div>
+		<section className='w-full max-w-sm p-10 rounded-xl border border-orange-300'>
+			<h1 className='text-3xl mb-8 font-bold font-mono text-center text-orange-600'>
+				Login via OTP
+			</h1>
+			<form className='flex flex-col gap-4 flex-1'>
+				<InputField
+					id='mobile-number'
+					label='Mobile Number'
+					name='mobileNumber'
+					placeholder='Enter mobile number'
+					type='text'
+				/>
+				<Button title='Send OTP' />
+			</form>
+		</section>
 	)
 }
 
