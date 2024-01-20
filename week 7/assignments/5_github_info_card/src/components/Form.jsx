50dfcdda6813c44388e09d6f125cc6ff6decb52f
anoopraju31 <anoopraju31@gmail.com>
2024-01-20 20:47:17 +0530

    Week 7 - Assignment - 5. Github Info Card - Form Component - submit functionality added

--- week 7/assignments/5_github_info_card/src/components/Form.jsx
+++ week 7/assignments/5_github_info_card/src/components/Form.jsx
@@ -1,11 +1,44 @@
+import { useCallback, useState } from 'react'
+import { useSetRecoilState } from 'recoil'
 import Button from './Button'
 import InputField from './InputField'
+import { usernameAtom } from '../store/atoms'
 
 const Form = () => {
+	const [username, setUsername] = useState('')
+	const [isSubmitting, setIsSubmitting] = useState(false)
+	const setUsernameToAtom = useSetRecoilState(usernameAtom)
+
+	const handleChange = useCallback((e) => setUsername(e.target.value), [])
+	const handleSubmit = useCallback(
+		(e) => {
+			try {
+				e.preventDefault()
+				setIsSubmitting(true)
+				setUsernameToAtom(username)
+			} catch (error) {
+				console.error(error)
+			} finally {
+				setUsername('')
+				setIsSubmitting(false)
+			}
+		},
+		[username, setUsernameToAtom],
+	)
+
 	return (
-		<form className='flex flex-col gap-4'>
-			<InputField />
-			<Button title='Search' />
+		<form onSubmit={handleSubmit} className='flex flex-col gap-4'>
+			<InputField
+				name='username'
+				id='username'
+				type='text'
+				placeholder='Enter username'
+				label='Username'
+				value={username}
+				handleChange={handleChange}
+				isDisabled={isSubmitting}
+			/>
+			<Button isDisabled={isSubmitting} title='Search' />
 		</form>
 	)
 }
