9df6d0760fb263409b9247b487e20ab59e494c2d
anoopraju31 <anoopraju31@gmail.com>
2024-01-21 19:36:44 +0530

    Week 8.2 - PayTM Clone backend - cors added

--- week 8/week-8.2/backend/index.js
+++ week 8/week-8.2/backend/index.js
@@ -1,8 +1,10 @@
 const express = require('express')
 const mainRoutes = require('./routes/index')
+const cors = require('cors')
 const app = express()
 const PORT = 5500
 
+app.use(cors())
 app.use('/api/v1', mainRoutes)
 
 app.listen(PORT, () => {
