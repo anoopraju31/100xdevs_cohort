cc338826cbd03bbafa0488cdaa79cfc6f2e92e55
anoopraju31 <anoopraju31@gmail.com>
2024-01-21 19:55:40 +0530

    Week 8.2 - PayTM Clone backend - signup route added

--- week 8/week-8.2/backend/routes/userRoutes.js
+++ week 8/week-8.2/backend/routes/userRoutes.js
@@ -1,8 +1,51 @@
 const express = require('express')
+const { signupSchema } = require('../helpers/validations')
+const jwt = require('jsonwebtoken')
+const { User } = require('../db')
+require('dotenv').config()
+
 const router = express.Router()
 
 router.get('/', (req, res) => {
 	res.json({ message: 'user routes' })
+})
+
+router.post('/sign-up', async (req, res) => {
+	const { username, firstName, lastName, password } = req.body
+	const { success } = signupSchema.safeParse(req.body)
+
+	if (!success)
+		return res.status(411).json({
+			message: 'Email already taken / Incorrect inputs',
+		})
+
+	const existingUser = await User.findOne({
+		username: req.body.username,
+	})
+
+	if (existingUser)
+		return res.status(411).json({
+			message: 'Email already taken/Incorrect inputs',
+		})
+
+	const user = await User.create({
+		username,
+		password,
+		firstName,
+		lastName,
+	})
+	const userId = user._id
+	const token = jwt.sign(
+		{
+			userId,
+		},
+		process.env.JWT_SECRET,
+	)
+
+	res.json({
+		message: 'User created successfully',
+		token: token,
+	})
 })
 
 module.exports = router
