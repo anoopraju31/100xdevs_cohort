cd81088869a0eb5c4ca6d98e1b5f80054a7f5759
anoopraju31 <anoopraju31@gmail.com>
2024-01-21 12:51:35 +0530

    Week 7.4 Atom Family - Todo Component - styled

--- week 7/week-7.4/atom-family/src/components/Todo.jsx
+++ week 7/week-7.4/atom-family/src/components/Todo.jsx
@@ -6,8 +6,8 @@ const Todo = ({ id }) => {
 	const { title, description } = useRecoilValue(todoAtom)
 	console.log(id)
 	return (
-		<div>
-			<h5> {title} </h5>
+		<div className='p-5 flex flex-col justify-center items-center gap-3 bg-orange-100 rounded-xl'>
+			<h5 className='font-semibold text-xl'> {title} </h5>
 			<p> {description} </p>
 		</div>
 	)
