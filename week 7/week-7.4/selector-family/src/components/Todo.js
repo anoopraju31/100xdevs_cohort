81e2e965ed74c5f6eb4a47ee2a0882070d32efa6
anoopraju31 <anoopraju31@gmail.com>
2024-01-21 13:57:14 +0530

    Week 7.4 - Selector Family - Home Page - styled

--- week 7/week-7.4/selector-family/src/components/Todo.js
+++
@@ -1,5 +0,0 @@
-const Todo = () => {
-	return <div>Todo</div>
-}
-
-export default Todo
--- week 7/week-7.4/selector-family/src/pages/Home.jsx
+++ week 7/week-7.4/selector-family/src/pages/Home.jsx
@@ -1,5 +1,17 @@
+import Todo from '../components/Todo'
+
 const Home = () => {
-	return <div>Home</div>
+	return (
+		<main className='min-h-screen flex flex-col items-center gap-5 bg-orange-50 p-10'>
+			<Todo id={1} />
+			<Todo id={2} />
+			<Todo id={2} />
+			<Todo id={2} />
+			<Todo id={2} />
+			<Todo id={2} />
+			<Todo id={2} />
+		</main>
+	)
 }
 
 export default Home
