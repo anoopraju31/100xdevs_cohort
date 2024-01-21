db6ac1e8a22ca701ad3f3dc895a46f2f57121304
anoopraju31 <anoopraju31@gmail.com>
2024-01-21 19:26:55 +0530

    Week 8.2 - PayTM Clone backend - User router created

--- week 8/week-8.2/backend/index.js
+++ week 8/week-8.2/backend/index.js
@@ -1,10 +1,13 @@
 const express = require('express')
+const userRoutes = require('./routes/userRoutes.js')
 const app = express()
 const PORT = 5500
 
 app.get('/', (req, res) => {
 	res.json({ message: 'Welcome to PayTM clone' })
 })
+
+app.use('/users', userRoutes)
 
 app.listen(PORT, () => {
 	console.log(`server started at port ${PORT}.`)
