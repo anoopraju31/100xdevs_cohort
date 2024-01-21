ff44f11b3ab8ea2b5865cf7a5d7a6a0023fab66d
anoopraju31 <anoopraju31@gmail.com>
2024-01-21 20:32:07 +0530

    Week 8.2 - PayTM Clone backend - Account Model added

--- week 8/week-8.2/backend/db.js
+++ week 8/week-8.2/backend/db.js
@@ -32,8 +32,22 @@ const userSchema = new mongoose.Schema({
 	},
 })
 
+const accountSchema = new mongoose.Schema({
+	userId: {
+		type: mongoose.Schema.Types.ObjectId, // Reference to User model
+		ref: 'User',
+		required: true,
+	},
+	balance: {
+		type: Number,
+		required: true,
+	},
+})
+
 const User = mongoose.model('User', userSchema)
+const Account = mongoose.model('Account', accountSchema)
 
 module.exports = {
 	User,
+	Account,
 }
