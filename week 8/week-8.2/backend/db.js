b172ff428c4654163ecc44369be7bfd701cf801a
anoopraju31 <anoopraju31@gmail.com>
2024-01-21 19:22:40 +0530

    Week 8.2 - PayTM Clone backend - User model created

---
+++ week 8/week-8.2/backend/db.js
@@ -0,0 +1,39 @@
+const mongoose = require('mongoose')
+require('dotenv').config()
+
+mongoose.connect(process.env.MONGODB_URI)
+
+const userSchema = new mongoose.Schema({
+	username: {
+		type: String,
+		required: true,
+		unique: true,
+		trim: true,
+		lowercase: true,
+		minLength: 3,
+		maxLength: 30,
+	},
+	password: {
+		type: String,
+		required: true,
+		minLength: 6,
+	},
+	firstName: {
+		type: String,
+		required: true,
+		trim: true,
+		maxLength: 50,
+	},
+	lastName: {
+		type: String,
+		required: true,
+		trim: true,
+		maxLength: 50,
+	},
+})
+
+const User = mongoose.model('User', userSchema)
+
+module.exports = {
+	User,
+}
