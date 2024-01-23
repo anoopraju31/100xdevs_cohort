f4539f8139b2005669c74d98f6eee4c63d9867aa
anoopraju31 <anoopraju31@gmail.com>
2024-01-23 14:44:14 +0530

    Week 8.1 - story book tutorial - Buutton Component - initial

--- week 8/week-8.1/story-book-tutorial/src/App.jsx
+++ week 8/week-8.1/story-book-tutorial/src/App.jsx
@@ -1,41 +1,9 @@
-import Card from './components/Card'
+import Button from './components/Button'
 
 const App = () => {
 	return (
 		<div className='max-w-sm mx-auto my-20 flex flex-col gap-5'>
-			<Card
-				title='Noteworthy technology acquisitions 2021'
-				description='Here are the biggest enterprise technology acquisitions of 2021 so far, in reverse chronological order.'
-				image='https://flowbite.com/docs/images/blog/image-1.jpg'
-				altText='technology'
-				link='https://flowbite.com/docs/components/card/'
-				linkText='See our guideline'
-			/>
-
-			<Card
-				title='Noteworthy technology acquisitions 2021'
-				description='Here are the biggest enterprise technology acquisitions of 2021 so far, in reverse chronological order.'
-				image='https://flowbite.com/docs/images/blog/image-1.jpg'
-				altText='technology'
-				link='https://flowbite.com/docs/components/card/'
-				linkText='See our guideline'
-				isButton={true}
-			/>
-
-			<Card
-				title='Noteworthy technology acquisitions 2021'
-				description='Here are the biggest enterprise technology acquisitions of 2021 so far, in reverse chronological order.'
-				link='https://flowbite.com/docs/components/card/'
-				linkText='See our guideline'
-				isButton={true}
-			/>
-
-			<Card
-				title='Noteworthy technology acquisitions 2021'
-				description='Here are the biggest enterprise technology acquisitions of 2021 so far, in reverse chronological order.'
-				link='https://flowbite.com/docs/components/card/'
-				linkText='See our guideline'
-			/>
+			<Button />
 		</div>
 	)
 }
