15e55a79deaf8e0827ffb417e0d2a9d807f179b7
anoopraju31 <anoopraju31@gmail.com>
2024-01-23 18:33:06 +0530

    Week 8.1 - Dukaan Dashboard - custom colors added to tailwind config

--- week 8/week-8.1/dukaan-dashboard/tailwind.config.js
+++ week 8/week-8.1/dukaan-dashboard/tailwind.config.js
@@ -2,7 +2,25 @@
 export default {
 	content: ['./index.html', './src/**/*.{js,jsx}'],
 	theme: {
-		extend: {},
+		extend: {
+			colors: {
+				'black-98': '#FAFAFA',
+				'black-100': '#ffffff',
+				'black-12': '#1a181e',
+				'black-50': '#808080',
+				'black-60': '#999999',
+				'black-90': '#e6e6e6',
+				'black-95': '#f2f2f2',
+				'black-30': '#4d4d4d',
+				'black-85': '#d9d9d9',
+				'hover-blue': '#0e4f82',
+				'primary-blue': '#146eb4',
+				green: '#17b31b',
+				navbar: '#1e2640',
+				'navbar-hover': 'rgba(255, 255, 255, 0.10)',
+				'navbar-secondary': '#353c53',
+			},
+		},
 	},
 	plugins: [],
 }
