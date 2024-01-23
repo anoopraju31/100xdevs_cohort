8aa0cb17782ea9c1102b423c6be5f6931d89c216
anoopraju31 <anoopraju31@gmail.com>
2024-01-23 16:20:31 +0530

    Week 8.1 - story book tutorial - Button Story added

---
+++ week 8/week-8.1/story-book-tutorial/src/stories/Button.stories.jsx
@@ -0,0 +1,75 @@
+import Button from '../components/Button'
+
+const meta = {
+	component: Button,
+}
+
+export const PrimaryButton = {
+	args: {
+		title: 'Button',
+		variant: 'primary',
+	},
+}
+
+export const SecondaryButton = {
+	args: {
+		title: 'Button',
+		variant: 'secondary',
+	},
+}
+
+export const DangerButton = {
+	args: {
+		title: 'Button',
+		variant: 'danger',
+	},
+}
+
+export const SuccessButton = {
+	args: {
+		title: 'Button',
+		variant: 'success',
+	},
+}
+
+export const ExtraSmallPrimaryButton = {
+	args: {
+		title: 'Button',
+		variant: 'primary',
+		size: 'xs',
+	},
+}
+
+export const SmallPrimaryButton = {
+	args: {
+		title: 'Button',
+		variant: 'primary',
+		size: 'sm',
+	},
+}
+
+export const LargePrimaryButton = {
+	args: {
+		title: 'Button',
+		variant: 'primary',
+		size: 'lg',
+	},
+}
+
+export const ExtraLargePrimaryButton = {
+	args: {
+		title: 'Button',
+		variant: 'primary',
+		size: 'xl',
+	},
+}
+
+export const PrimaryButtonDisabled = {
+	args: {
+		title: 'Button',
+		variant: 'primary',
+		disabled: true,
+	},
+}
+
+export default meta
