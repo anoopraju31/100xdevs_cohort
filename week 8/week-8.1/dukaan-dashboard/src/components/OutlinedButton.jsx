48b2a0bc4f32df8b7fde2abe949d21224a15aec0
anoopraju31 <anoopraju31@gmail.com>
2024-01-24 22:10:25 +0530

    Week 8.1 - Dukaan Dashbaord: Outlined Button Component - styled

--- week 8/week-8.1/dukaan-dashboard/src/components/OutlinedButton.jsx
+++ week 8/week-8.1/dukaan-dashboard/src/components/OutlinedButton.jsx
@@ -1,5 +1,19 @@
-const OutlinedButton = () => {
-	return <div>OutlinedButton</div>
+import PropTypes from 'prop-types'
+
+const OutlinedButton = ({ children, handleClick }) => {
+	return (
+		<button
+			type='button'
+			onClick={handleClick}
+			className='py-[6px] px-3 flex items-center gap-[6px] rounded border border-black-85 bg-black-100'>
+			{children}
+		</button>
+	)
+}
+
+OutlinedButton.propTypes = {
+	children: PropTypes.node,
+	handleClick: PropTypes.func,
 }
 
 export default OutlinedButton
