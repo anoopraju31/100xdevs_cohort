7555b4d47ed5bfa760016ec6b84fb6d77ecf8b3f
anoopraju31 <anoopraju31@gmail.com>
2024-01-24 17:11:00 +0530

    Week 8.1 - Dukaan Dashbaord: Dashboard Component - initial

--- week 8/week-8.1/dukaan-dashboard/src/App.jsx
+++ week 8/week-8.1/dukaan-dashboard/src/App.jsx
@@ -1,9 +1,17 @@
+import Navbar from './components/Navbar'
 import Header from './components/Header'
+import Dashboard from './components/Dashboard'
 
 const App = () => {
 	return (
-		<div className='h-[200vh]'>
-			<Header />
+		<div className='flex'>
+			<div className='hidden lg:block'>
+				<Navbar />
+			</div>
+			<div className='flex-1 relative'>
+				<Header />
+				<Dashboard />
+			</div>
 		</div>
 	)
 }
