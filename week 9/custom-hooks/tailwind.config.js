f77d36bf779dfe2d8c51e0302457b2e01451c8a4
anoopraju31 <anoopraju31@gmail.com>
2024-01-27 19:56:46 +0530

    Week 9.1 Custom Hooks - tailwind css added

--- week 9/custom-hooks/tailwind.config.js
+++ week 9/custom-hooks/tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: ['./index.html', './src/**/*.{js,jsx'],
+	content: ['./index.html', './src/**/*.{js,jsx}'],
 	theme: {
 		extend: {},
 	},
