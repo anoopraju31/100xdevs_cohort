eeb58077ce7837616fb63a4856eac65c10b678a4
anoopraju31 <anoopraju31@gmail.com>
2024-01-27 22:48:27 +0530

    Week 9.1 Custom Hook - Form Component - styled

--- week 9/custom-hooks/src/components/Form.jsx
+++ week 9/custom-hooks/src/components/Form.jsx
@@ -1,5 +1,28 @@
+import { useState } from 'react'
+import InputField from './InputField'
+import useDebounce from '../hooks/useDebounce'
+
 const Form = () => {
-	return <div>Form</div>
+	const [input, setInput] = useState('')
+	const handleChange = (e) => setInput(e.target.value)
+	const debounceHandler = () => {
+		if (!input) return
+		alert(input)
+	}
+	useDebounce(debounceHandler, 200)
+
+	return (
+		<div>
+			<InputField
+				placeholder='search'
+				id='search'
+				name='search'
+				label='search'
+				value={input}
+				handleChange={handleChange}
+			/>
+		</div>
+	)
 }
 
 export default Form
