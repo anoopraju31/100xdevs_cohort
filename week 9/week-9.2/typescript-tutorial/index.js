1192fd4a3c1d2d1036ff3f9a7ef490fd00188bf5
anoopraju31 <anoopraju31@gmail.com>
2024-01-28 19:43:44 +0530

    Week 9.2 typescript tutorial - function as argument to function

--- week 9/week-9.2/typescript-tutorial/index.js
+++ week 9/week-9.2/typescript-tutorial/index.js
@@ -1,11 +1,18 @@
-var x = 101;
+"use strict";
+const x = 101;
 console.log(x);
 //  functions
 function greet(name) {
-    return "Welcome ".concat(name, "!");
+    console.log(`Welcome ${name}!`);
 }
+greet('Anoop');
 function sum(num1, num2) {
     return num1 + num2;
 }
-console.log(greet('Anoop'));
 console.log(sum(10, 24));
+function runAfter1s(fn) {
+    setTimeout(fn, 1000);
+}
+runAfter1s(function () {
+    console.log('Hi there!');
+});
--- week 9/week-9.2/typescript-tutorial/index.ts
+++ week 9/week-9.2/typescript-tutorial/index.ts
@@ -4,12 +4,21 @@ console.log(x)
 
 //  functions
 function greet(name: string) {
-	return `Welcome ${name}!`
+	console.log(`Welcome ${name}!`)
 }
 
-function sum(num1: number, num2: number) {
+greet('Anoop')
+
+function sum(num1: number, num2: number): number {
 	return num1 + num2
 }
 
-console.log(greet('Anoop'))
 console.log(sum(10, 24))
+
+function runAfter1s(fn: () => void) {
+	setTimeout(fn, 1000)
+}
+
+runAfter1s(function () {
+	console.log('Hi there!')
+})
