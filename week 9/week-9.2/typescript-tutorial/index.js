faeef46970566168c902b952c9b1588becbfc545
anoopraju31 <anoopraju31@gmail.com>
2024-01-28 19:34:04 +0530

    Week 9.2 typescript tutorial - function

--- week 9/week-9.2/typescript-tutorial/index.js
+++ week 9/week-9.2/typescript-tutorial/index.js
@@ -1,2 +1,11 @@
 var x = 101;
 console.log(x);
+//  functions
+function greet(name) {
+    return "Welcome ".concat(name, "!");
+}
+function sum(num1, num2) {
+    return num1 + num2;
+}
+console.log(greet('Anoop'));
+console.log(sum(10, 24));
--- week 9/week-9.2/typescript-tutorial/index.ts
+++ week 9/week-9.2/typescript-tutorial/index.ts
@@ -1,3 +1,15 @@
 const x: number = 101
 
 console.log(x)
+
+//  functions
+function greet(name: string) {
+	return `Welcome ${name}!`
+}
+
+function sum(num1: number, num2: number) {
+	return num1 + num2
+}
+
+console.log(greet('Anoop'))
+console.log(sum(10, 24))
