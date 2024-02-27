5d4c0bacc07a5ab1a0bf23be528fb19de4fe99ea
anoopraju31 <anoopraju31@gmail.com>
2024-02-27 22:15:48 +0530

    Week 11.1 - Hono app tutorial - post request

--- week 11/week-11.1/my-app/src/index.ts
+++ week 11/week-11.1/my-app/src/index.ts
@@ -2,8 +2,13 @@ import { Hono } from 'hono'
 
 const app = new Hono()
 
-app.get('/', (c) => {
-  return c.text('Hello Hono!')
+app.post('/', async (c) => {
+	const body = await c.req.json()
+	console.log(body)
+	console.log(c.req.header('Authorization'))
+	console.log(c.req.query('param'))
+
+	return c.text('Hello Hono!')
 })
 
 export default app
