9316f37836a2ee5d14edda49ed1d991e0d56380d
anoopraju31 <anoopraju31@gmail.com>
2024-02-28 15:22:01 +0530

    Week 11.1 - Hono tutorial - middleware

--- week 11/week-11.1/my-app/src/index.ts
+++ week 11/week-11.1/my-app/src/index.ts
@@ -14,20 +14,21 @@
 // export default app
 
 import { Hono, Next } from 'hono'
-import { Context } from 'hono/jsx'
 
 const app = new Hono()
 
-app.use(async (c, next) => {
+const authMiddleware = async (c: any, next: Next) => {
 	if (c.req.header('Authorization')) {
 		// Do validation
 		await next()
 	} else {
 		return c.text('You dont have acces')
 	}
-})
+}
 
-app.get('/', async (c) => {
+app.use(authMiddleware)
+
+app.get('/', authMiddleware, async (c) => {
 	const body = await c.req.parseBody()
 	console.log(body)
 	console.log(c.req.header('Authorization'))
