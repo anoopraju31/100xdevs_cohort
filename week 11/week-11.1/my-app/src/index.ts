2e27592d926f4fb8b6e1c869557032d4e7b6adce
anoopraju31 <anoopraju31@gmail.com>
2024-02-28 15:15:14 +0530

    Week 11.1 - Hono tutorial - middleware

--- week 11/week-11.1/my-app/src/index.ts
+++ week 11/week-11.1/my-app/src/index.ts
@@ -1,14 +1,39 @@
-import { Hono } from 'hono'
+// import { Hono } from 'hono'
+
+// const app = new Hono()
+
+// app.post('/', async (c) => {
+// 	const body = await c.req.json()
+// 	console.log(body)
+// 	console.log(c.req.header('Authorization'))
+// 	console.log(c.req.query('param'))
+
+// 	return c.text('Hello Hono!')
+// })
+
+// export default app
+
+import { Hono, Next } from 'hono'
+import { Context } from 'hono/jsx'
 
 const app = new Hono()
 
-app.post('/', async (c) => {
-	const body = await c.req.json()
+app.use(async (c, next) => {
+	if (c.req.header('Authorization')) {
+		// Do validation
+		await next()
+	} else {
+		return c.text('You dont have acces')
+	}
+})
+
+app.get('/', async (c) => {
+	const body = await c.req.parseBody()
 	console.log(body)
 	console.log(c.req.header('Authorization'))
 	console.log(c.req.query('param'))
 
-	return c.text('Hello Hono!')
+	return c.json({ msg: 'as' })
 })
 
 export default app
