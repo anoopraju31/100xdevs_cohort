3393de02dcad2b4b5e3380281d41eae44875339a
anoopraju31 <anoopraju31@gmail.com>
2024-03-06 11:10:18 +0530

    Week 14.2 - Data Fetching Nextjs - create own backend route for user details

---
+++ week 14/week-14.2/data-fetching-next/app/api/user/route.js
@@ -0,0 +1,8 @@
+export function GET() {
+	// Database Logic
+
+	return Response.json({
+		email: 'anoopraju31@gmail.com',
+		name: 'Anoop Raju',
+	})
+}
--- week 14/week-14.2/data-fetching-next/app/page.js
+++ week 14/week-14.2/data-fetching-next/app/page.js
@@ -1,10 +1,7 @@
 import axios from 'axios'
 
 async function getUserDetails() {
-	await new Promise((r) => setTimeout(r, 5000))
-	const response = await axios.get(
-		'https://week-13-offline.kirattechnologies.workers.dev/api/v1/user/details',
-	)
+	const response = await axios.get('http://localhost:3000/api/user')
 	return response.data
 }
 
