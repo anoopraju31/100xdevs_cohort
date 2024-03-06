b37a0ad6c00dfed41fbc8577b5230656cd2eb85b
anoopraju31 <anoopraju31@gmail.com>
2024-03-06 13:08:08 +0530

    Week 14.2 - SignUp app - sign up route with db

--- week 14/week-14.2/signup-app/app/api/user/route.ts
+++ week 14/week-14.2/signup-app/app/api/user/route.ts
@@ -1,9 +1,24 @@
 import { NextRequest, NextResponse } from 'next/server'
+import { PrismaClient } from '@prisma/client'
+
+const client = new PrismaClient()
 
 export async function POST(req: NextRequest) {
 	const body = await req.json()
+	// should add zod validation here
+	const user = await client.user.create({
+		data: {
+			username: body.username,
+			password: body.password,
+		},
+	})
 
-	console.log(body)
+	console.log(user.id)
 
-	return NextResponse.json({ username: body.username, password: body.password })
+	return NextResponse.json({ message: 'Signed up' })
+}
+
+export async function GET() {
+	const user = await client.user.findFirst({})
+	return Response.json({ name: user?.username, email: user?.username })
 }
