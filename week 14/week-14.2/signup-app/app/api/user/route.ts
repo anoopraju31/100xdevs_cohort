2fcc92601eb1f0eb6fb99cc0466642ba54546d6f
anoopraju31 <anoopraju31@gmail.com>
2024-03-06 12:51:17 +0530

    Week 14.2 - SignUp App - sign up route initial

---
+++ week 14/week-14.2/signup-app/app/api/user/route.ts
@@ -0,0 +1,9 @@
+import { NextRequest, NextResponse } from 'next/server'
+
+export async function POST(req: NextRequest) {
+	const body = await req.json()
+
+	console.log(body)
+
+	return NextResponse.json({ username: body.username, password: body.password })
+}
