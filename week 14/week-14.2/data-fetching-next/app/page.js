4cbfa587dac2772583aece77fd3500b5c14671a4
anoopraju31 <anoopraju31@gmail.com>
2024-03-06 10:49:47 +0530

    Week 14.2 - Data Fetching Nextjs - loader added

--- week 14/week-14.2/data-fetching-next/app/page.js
+++ week 14/week-14.2/data-fetching-next/app/page.js
@@ -1,24 +1,25 @@
-import axios from "axios";
+import axios from 'axios'
 
 async function getUserDetails() {
-  const response = await axios.get("https://week-13-offline.kirattechnologies.workers.dev/api/v1/user/details")
-	return response.data;
+	await new Promise((r) => setTimeout(r, 5000))
+	const response = await axios.get(
+		'https://week-13-offline.kirattechnologies.workers.dev/api/v1/user/details',
+	)
+	return response.data
 }
 
 export default async function Home() {
-  const userData = await getUserDetails();
+	const userData = await getUserDetails()
 
-  return (
-    <div className="flex flex-col justify-center h-screen">
-        <div className="flex justify-center">
-            <div className="border p-8 rounded">
-                <div>
-                    Name: {userData?.name}
-                </div>
-                
-                {userData?.email}
-            </div>
-        </div>
-    </div>
-  );
+	return (
+		<div className='flex flex-col justify-center h-screen'>
+			<div className='flex justify-center'>
+				<div className='border p-8 rounded'>
+					<div>Name: {userData?.name}</div>
+
+					{userData?.email}
+				</div>
+			</div>
+		</div>
+	)
 }
