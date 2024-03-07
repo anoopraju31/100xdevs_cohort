2c73e3a3e02d87b85d6252066cffc370fb2a338b
anoopraju31 <anoopraju31@gmail.com>
2024-03-07 21:13:04 +0530

    Week 13.1 - Medium Clone - Backend - Sign Up Route added

--- week 13/week-13.1/medium-clone/backend/src/index.ts
+++ week 13/week-13.1/medium-clone/backend/src/index.ts
@@ -1,9 +1,37 @@
 import { Hono } from 'hono'
+import { PrismaClient } from '@prisma/client/edge'
+import { withAccelerate } from '@prisma/extension-accelerate'
+import { sign } from 'hono/jwt'
 
-const app = new Hono()
+const app = new Hono<{
+	Bindings: {
+		DATABASE_URL: string
+		JWT_SECRET: string
+	}
+}>()
 
-app.post('/api/v1/signup', (c) => {
-	return c.text('sign up route')
+app.post('/api/v1/signup', async (c) => {
+	const prisma = new PrismaClient({
+		datasourceUrl: c.env?.DATABASE_URL,
+	}).$extends(withAccelerate())
+
+	const body = await c.req.json()
+
+	try {
+		const user = await prisma.user.create({
+			data: {
+				email: body.email,
+				password: body.password,
+			},
+		})
+
+		const jwt = await sign({ id: user.id }, c.env.JWT_SECRET)
+
+		return c.json({ jwt })
+	} catch (error) {
+		c.status(403)
+		return c.json({ error: 'error while signing up' })
+	}
 })
 
 app.post('/api/v1/signin', (c) => {
