c24d541258614ceb66d713baec05596769516737
anoopraju31 <anoopraju31@gmail.com>
2024-03-07 22:13:03 +0530

    Week 13.1 - Medium Clone - Backend - Sign In Route added

--- week 13/week-13.1/medium-clone/backend/src/index.ts
+++ week 13/week-13.1/medium-clone/backend/src/index.ts
@@ -34,8 +34,27 @@ app.post('/api/v1/signup', async (c) => {
 	}
 })
 
-app.post('/api/v1/signin', (c) => {
-	return c.text('sign in route')
+app.post('/api/v1/signin', async (c) => {
+	const prisma = new PrismaClient({
+		datasourceUrl: c.env?.DATABASE_URL,
+	}).$extends(withAccelerate())
+
+	const body = await c.req.json()
+
+	const user = await prisma.user.findUnique({
+		where: {
+			email: body.email,
+		},
+	})
+
+	if (!user) {
+		c.status(403)
+		return c.json({ error: 'user not found' })
+	}
+
+	const jwt = await sign({ id: user.id }, c.env.JWT_SECRET)
+
+	return c.json({ jwt })
 })
 
 app.post('/api/v1/blog', (c) => {
