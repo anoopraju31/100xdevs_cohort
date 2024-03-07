71dcdca631411146c120be5f2360b22b245d09dc
anoopraju31 <anoopraju31@gmail.com>
2024-03-07 20:15:06 +0530

    Week 13.1 - Medium project- backend - Initialize handlers

--- week 13/week-13.1/medium-clone/backend/src/index.ts
+++ week 13/week-13.1/medium-clone/backend/src/index.ts
@@ -2,8 +2,32 @@ import { Hono } from 'hono'
 
 const app = new Hono()
 
-app.get('/', (c) => {
-  return c.text('Hello Hono!')
+app.post('/api/v1/signup', (c) => {
+	return c.text('sign up route')
+})
+
+app.post('/api/v1/signin', (c) => {
+	return c.text('sign in route')
+})
+
+app.post('/api/v1/blog', (c) => {
+	return c.text('create blog route')
+})
+
+app.put('/api/v1/blog', (c) => {
+	return c.text('edit blog')
+})
+
+app.get('/api/v1/blog/:id', (c) => {
+	const id = c.req.param('id')
+
+	console.log(id)
+
+	return c.text('get blog by id route')
+})
+
+app.get('/api/v1/blog/bulk', (c) => {
+	return c.text('get all blogs')
 })
 
 export default app
