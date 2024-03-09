7f7b83897c9f2cc191fa3ccfa517311df8d0d5e2
anoopraju31 <anoopraju31@gmail.com>
2024-03-09 18:05:25 +0530

    Week 12.2 ts-tutorial - pick

--- week 12/week-12.2/ts-tutorial/src/index.ts
+++ week 12/week-12.2/ts-tutorial/src/index.ts
@@ -1,11 +1,35 @@
+// interface User {
+// 	name: string
+// 	age: number
+// }
+
+// function sumOfAge(user1: User, user2: User) {
+// 	return user1.age + user2.age
+// }
+
+// const age = sumOfAge({ name: 'Taro', age: 20 }, { name: 'Jiro', age: 30 })
+// console.log(age)
+
 interface User {
+	id: string
 	name: string
 	age: number
+	email: string
+	password: string
 }
 
-function sumOfAge(user1: User, user2: User) {
-	return user1.age + user2.age
+type UserProfile = Pick<User, 'name' | 'email'>
+
+const displayUserProfile = (user: UserProfile) => {
+	console.log(`Name: ${user.name}, Email: ${user.email}`)
 }
 
-const age = sumOfAge({ name: 'Taro', age: 20 }, { name: 'Jiro', age: 30 })
-console.log(age)
+const user1: User = {
+	id: 'aq1wws3ed',
+	name: 'Harkirat Singh',
+	age: 30,
+	email: 'harkirat@gmail.com',
+	password: 'fjoitjpijgodmvompi',
+}
+
+displayUserProfile(user1)
